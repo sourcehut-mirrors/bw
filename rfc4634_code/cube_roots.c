14966a41b0d5cab594116fe8b4dff200928baa3c
blastwave <dclarke@blastwave.org>
2021-08-23 05:05:51 +0000

    20210823050551 : compute the fractional parts of the cube roots per RFC-4634

--- rfc4634_code/cube_roots.c
+++ rfc4634_code/cube_roots.c
@@ -69,9 +69,10 @@
 int main(int argc, char *argv[]) 
 {
 
-    double cuberoot = 0.0;
-    long double cuberoot_ld = 0.0L;
+    double frac, cuberoot = 0.0;
+    long double frac_ld, cuberoot_ld = 0.0L;
     int j = 0;
+    int k = 0;
 
     /* prime numbers to use */
     int p[80] = {   2,   3,   5,   7,  11,  13,  17,  19,  23,  29,
@@ -82,6 +83,39 @@ int main(int argc, char *argv[])
                   233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
                   283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
                   373, 379, 383, 389, 397, 401, 409, 419, 421, 431 };
+
+    /* this is clunky but gets the job done.
+     *
+     * our reference list of hex prime fractions as described
+     * in the comments above. */
+    char **hpf = (char *[]) {
+             "428A2F98D728AE22", "7137449123EF65CD", "B5C0FBCFEC4D3B2F",
+             "E9B5DBA58189DBBC", "3956C25BF348B538", "59F111F1B605D019",
+             "923F82A4AF194F9B", "AB1C5ED5DA6D8118", "D807AA98A3030242",
+             "12835B0145706FBE", "243185BE4EE4B28C", "550C7DC3D5FFB4E2",
+             "72BE5D74F27B896F", "80DEB1FE3B1696B1", "9BDC06A725C71235",
+             "C19BF174CF692694", "E49B69C19EF14AD2", "EFBE4786384F25E3",
+             "0FC19DC68B8CD5B5", "240CA1CC77AC9C65", "2DE92C6F592B0275",
+             "4A7484AA6EA6E483", "5CB0A9DCBD41FBD4", "76F988DA831153B5",
+             "983E5152EE66DFAB", "A831C66D2DB43210", "B00327C898FB213F",
+             "BF597FC7BEEF0EE4", "C6E00BF33DA88FC2", "D5A79147930AA725",
+             "06CA6351E003826F", "142929670A0E6E70", "27B70A8546D22FFC",
+             "2E1B21385C26C926", "4D2C6DFC5AC42AED", "53380D139D95B3DF",
+             "650A73548BAF63DE", "766A0ABB3C77B2A8", "81C2C92E47EDAEE6",
+             "92722C851482353B", "A2BFE8A14CF10364", "A81A664BBC423001",
+             "C24B8B70D0F89791", "C76C51A30654BE30", "D192E819D6EF5218",
+             "D69906245565A910", "F40E35855771202A", "106AA07032BBD1B8",
+             "19A4C116B8D2D0C8", "1E376C085141AB53", "2748774CDF8EEB99",
+             "34B0BCB5E19B48A8", "391C0CB3C5C95A63", "4ED8AA4AE3418ACB",
+             "5B9CCA4F7763E373", "682E6FF3D6B2B8A3", "748F82EE5DEFB2FC",
+             "78A5636F43172F60", "84C87814A1F0AB72", "8CC702081A6439EC",
+             "90BEFFFA23631E28", "A4506CEBDE82BDE9", "BEF9A3F7B2C67915",
+             "C67178F2E372532B", "CA273ECEEA26619C", "D186B8C721C0C207",
+             "EADA7DD6CDE0EB1E", "F57D4F7FEE6ED178", "06F067AA72176FBA",
+             "0A637DC5A2C898A6", "113F9804BEF90DAE", "1B710B35131C471B",
+             "28DB77F523047D84", "32CAAB7B40C72493", "3C9EBE0A15C9BEBC",
+             "431D67C49C100D4C", "4CC5D4BECB3E42B6", "597F299CFC657E2A",
+             "5FCB6FAB3AD6FAEC", "6C44198C4A475817" };
 
     char *buf = setlocale ( LC_ALL, "POSIX" );
     if ( buf == NULL ) {
@@ -111,13 +145,28 @@ int main(int argc, char *argv[])
      *
      */
 
-    printf ("\n---------------- 64-bit double type ---------------\n");
-    for ( j=0; j<80; j++ ) {
+    printf ("\nExpect this to fail terribly as we lose precision\n");
+    printf ("---------------- 64-bit double type ---------------\n");
+    printf ("    p    64-bit cube root                Reference Hex       Computed Hex\n");
+    printf ("-------------------------------------------------------------------------\n");
+    for ( j=0; j<8; j++ ) {
         cuberoot = exp(log((double)p[j])/3.0);
-        printf ("  %3i    %-28.20e\n", p[j], cuberoot);
+        printf ("  %3i    %-28.20e    %s    ", p[j], cuberoot, hpf[j]);
+        frac = ( cuberoot - floor(cuberoot) ) * 16.0;
+        /* printf ("\n\n            frac = %-28.20e\n\n", frac); */
+        for ( k=0; k<16; k++ ) {
+            /* printf ("\n\n            %i        %i\n",k,(int)frac); */
+            printf ("%1X", (int)frac);
+            frac = frac - floor(frac);
+            frac = frac * 16.0;
+        }
+        printf ("\n");
     }
 
-    printf ("---------------------------------------------------\n");
+    printf ("\nIf you are on x86 hardware this may not work.\n");
+    printf ("---------------- 128-bit double type? -------------\n");
+    printf ("    p    128-bit cube root                           Reference Hex       Computed Hex\n");
+    printf ("-----------------------------------------------------------------------------------------\n");
     /* note that Intel and AMD64 x86 hardware can not handle
      * the IEEE-754(2008) floating point standard. We are lucky
      * if we get 80 bits from that trash. Use gdb to confirm
@@ -130,9 +179,17 @@ int main(int argc, char *argv[])
      * a decent risc machine that can do IEEE-754 floating
      * point in the correct 128-bit datatype.
      */
-    for ( j=0; j<80; j++ ) {
+    for ( j=0; j<8; j++ ) {
         cuberoot_ld = expl(logl((long double)p[j])/3.0L);
-        printf ("  %3i    %-40.32Le\n", p[j], cuberoot_ld);
+        printf ("  %3i    %-40.32Le    %s    ", p[j], cuberoot_ld,hpf[j]);
+
+        frac_ld = ( cuberoot_ld - floorl(cuberoot_ld) ) * 16.0L;
+        for ( k=0; k<16; k++ ) {
+            printf ("%1X", (int)frac_ld);
+            frac_ld = frac_ld - floorl(frac_ld);
+            frac_ld = frac_ld * 16.0L;
+        }
+        printf ("\n");
     }
 
     return EXIT_SUCCESS;
