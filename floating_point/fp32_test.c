0398a3fc2f16cf60851ba7b0acb6e9a1bbcd7168
blastwave <dclarke@blastwave.org>
2021-08-31 00:53:44 +0000

    20210831005344 : demonstrate catastrophic loss of precision in fp32 data

--- floating_point/fp32_test.c
+++ floating_point/fp32_test.c
@@ -90,7 +90,148 @@ int main(int argc, char *argv[]){
     }
     printf ( "fp2 = fp0 + fp1 + tenth + dragon = %g\n", fp2 );
 
-    return ( EXIT_SUCCESS );
+
+
+
+    printf ("\n\n\n\n-----------------------------------\n\n");
+
+
+    /* set fp0 to 2 ^ 10 */
+    fp0 = 1024.0;
+    printf (" fp0 = 1024.0 done\n");
+
+    /* set fp1 to  2 ^ ( -10 ) */
+    fp1 = 1.0 / 1024.0;
+    printf (" fp1 = 1.0/1024.0 done\n");
+    
+    feclearexcept(FE_ALL_EXCEPT);
+    printf (" all floating point exception flags cleared\n");
+
+    fp2 = fp0 + fp1;
+    printf (" fp2 = fp0 + fp1 addition done\n");
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+    }
+    printf ("\n result of 1024 + ( 1 / 1024 ) = %-+24.18e\n", fp2 );
+
+
+
+    printf ("\n\n\n\n-----------------------------------\n\n");
+    /* set fp1 to  2 ^ ( -11 ) */
+    fp1 = 1.0 / 2048.0;
+    printf (" fp1 = 1.0/2048.0 = 2^(-11) done\n");
+    
+    feclearexcept(FE_ALL_EXCEPT);
+    printf (" all floating point exception flags cleared\n");
+
+    fp2 = fp0 + fp1;
+    printf (" fp2 = fp0 + fp1 addition done\n");
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+    }
+    printf ("\n result of 1024 + ( 1 / 2048 ) = %-+24.18e\n", fp2 );
+
+
+    printf ("\n\n\n\n-----------------------------------\n\n");
+    /* set fp1 to  2 ^ ( -12 ) */
+    fp1 = 1.0 / 4096.0;
+    printf (" fp1 = 1.0/4096.0 = 2^(-12) done\n");
+    
+    feclearexcept(FE_ALL_EXCEPT);
+    printf (" all floating point exception flags cleared\n");
+
+    fp2 = fp0 + fp1;
+    printf (" fp2 = fp0 + fp1 addition done\n");
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+    }
+    printf ("\n result of 1024 + ( 1 / 4096 ) = %-+24.18e\n", fp2 );
+
+
+    printf ("\n\n\n\n-----------------------------------\n\n");
+    /* set fp1 to  2 ^ ( -13 ) */
+    fp1 = 1.0 / 8192.0;
+    printf (" fp1 = 1.0/8192.0 = 2^(-13) done\n");
+    
+    feclearexcept(FE_ALL_EXCEPT);
+    printf (" all floating point exception flags cleared\n");
+
+    fp2 = fp0 + fp1;
+    printf (" fp2 = fp0 + fp1 addition done\n");
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+    }
+    printf ("\n result of 1024 + ( 1 / 8192 ) = %-+24.18e\n", fp2 );
+
+    printf ("\n\n\n\n-----------------------------------\n\n");
+    /* set fp1 to  2 ^ ( -14 ) */
+    fp1 = 1.0 / 16384.0;
+    printf (" fp1 = 1.0/16384.0 = 2^(-14) done\n");
+    printf (" fp1 = %-+24.18e\n", fp1 );
+    
+    feclearexcept(FE_ALL_EXCEPT);
+    printf (" all floating point exception flags cleared\n");
+
+    fp2 = fp0 + fp1;
+    printf (" fp2 = 1024 + fp1 addition done\n");
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+    }
+    printf ("We should expect to see catastrophic loss of precision\n\n");
+    printf ("\n result of 1024 + ( 1 / 16384 ) = %-+24.18e\n", fp2 );
+
+
+    return EXIT_SUCCESS;
 
 }
 
