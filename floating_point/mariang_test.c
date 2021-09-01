a6ca9b43b730670907258642b1aac30a60a3f458
blastwave <dclarke@blastwave.org>
2021-09-01 00:55:08 +0000

    20210901005508 : use MPFR to compute Rump(1988)

--- floating_point/mariang_test.c
+++ floating_point/mariang_test.c
@@ -1,31 +1,113 @@
 
+#define _XOPEN_SOURCE 600
+
 #include <stdio.h>
-#include <fenv.h>  
+#include <stdlib.h>
+#include <math.h>
+#include <fenv.h>
+#include <float.h>
 
-void test1()
-{
-    double const decimal =  1.0/20480;
-    double const integer = 1024.0L;
-    printf("000%.18f\n", decimal);
-    printf("%.18f\n", integer);
-    printf("%.18f\n", decimal + integer);
-    printf("\n");
-}
+int main(int argc, char *argv[]){
 
-void test2()
-{
-    double const decimal =  0.000048828125000000L;
-    double const integer = 1024.0L;
-    printf("000%.18f\n", decimal);
-    printf("%.18f\n", integer);
-    printf("%.18f\n", decimal + integer);
-    printf("\n");
-}
+    int fp_round_mode, fpe_raised;
 
-int main()
-{
-    test1();
-    test2();
-    return 0;
+#ifdef FLT_EVAL_METHOD
+    printf ( "INFO : FLT_EVAL_METHOD == %d\n", FLT_EVAL_METHOD);
+#endif
+
+    fp_round_mode = fegetround();
+    printf("INFO : fp rounding mode is ");
+    switch(fp_round_mode){
+        case FE_TONEAREST:
+            printf("FE_TONEAREST\n");
+            break;
+        case FE_TOWARDZERO:
+            printf("FE_TOWARDZERO\n");
+            break;
+        case FE_UPWARD:
+            printf("FE_UPWARD\n");
+            break;
+        case FE_DOWNWARD:
+            printf("FE_DOWNWARD\n");
+            break;
+        default:
+            printf("bloody unknown!\n");
+            break;
+        }
+
+    feclearexcept(FE_ALL_EXCEPT);
+
+    printf ("-----------------------------------\n\n");
+    printf ("Let's look at the number 5^11 / 10^12\n\n");
+
+    double numerator = 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0 * 5.0;
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    } else {
+        printf("INFO : no floating point exception\n");
+        printf("     : 5^11 computed precisely.\n");
+    }
+
+    printf("DATA : 5 ^ 11 = %-+24.18e\n\n", numerator);
+
+    feclearexcept(FE_ALL_EXCEPT);
+    double fraction = numerator / 1000000000000.0;
+
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    }
+
+    printf("DATA : ( 5^11 )/( 10^12 ) = %-+40.34e\n", fraction);
+
+    printf("\n\n  and now for something totally different\n\n");
+
+    feclearexcept(FE_ALL_EXCEPT);
+    double weird = 1024.0 + fraction;
+    fpe_raised = fetestexcept(FE_ALL_EXCEPT);
+    if (fpe_raised!=0){
+        printf("INFO : FP Exception raised is");
+        if(fpe_raised & FE_INEXACT) printf(" FE_INEXACT");
+        if(fpe_raised & FE_DIVBYZERO) printf(" FE_DIVBYZERO");
+        if(fpe_raised & FE_UNDERFLOW) printf(" FE_UNDERFLOW");
+        if(fpe_raised & FE_OVERFLOW) printf(" FE_OVERFLOW");
+        if(fpe_raised & FE_INVALID) printf(" FE_INVALID");
+        printf("\n");
+    }
+    printf("     : 1024 + ( 5^11 )/( 10^12 ) = %-+40.34e\n", weird);
+
+/***** I will clean up this comment later however the essential 
+ * data looks like so : 
+
+      0x4090 0x0000 0x0ccc 0xcccd
+        409 <-- sign bit and exponent bits
+
+           0   0000   0ccc   cccd
+
+                       1100 1100 1100 1100 1100 1100 1100 1100 ..... goes on forever
+
+         1.0000 0000 0000 0000 0000 0000 1100 1100 1100 1100 1100 1100 1101 <--- round up in ULP
+
+Previously we have ( 5^11 ) / ( 10^12 ) which was also a never ending
+binary expression : 0x3f09 0x9999 0x9999 0x999a  <--- round up in ULP
+
+*/
+
+    return EXIT_SUCCESS;
+
 }
 
--- floating_point/rump_mpfr.c
+++ floating_point/rump_mpfr.c
@@ -1,5 +1,4 @@
 
-
 /*
  * rump_mpfr.c A look at the Rump(1988) problem with arbitrary precision
  * Copyright (C) Dennis Clarke 2021
@@ -50,51 +49,290 @@
 #include <gmp.h>
 #include <mpfr.h>
 
-#define PREC 113 /* lowest reasonable precision */
+#define PREC 128 /* lowest reasonable precision */
 #define VERBOSE 1
 
 int mpfr_check_flags(int status, int debug_flag);
 
-int sysinfo(int VERBOSE);
-
 int main(int argc, char **argv)
 {
 
+    /* we may accept bit precision on the command line */
     long bit_prec = PREC;
-    long delta_bit_prec; /* used to increase precision if needed */
+
+    /* We may implement a method to detect loss of precision
+     * and then automatic adjustment of the variable bit width */
+    long delta_bit_prec;
     char *endptr, *str;
 
     setlocale( LC_ALL, "C" );
-    sysinfo(VERBOSE);
 
-    mpfr_t t0, t1, t2, t3, t4, t5, t6;
+    printf("GMP  library version : %d.%d.%d\n",
+            __GNU_MP_VERSION,
+            __GNU_MP_VERSION_MINOR,
+            __GNU_MP_VERSION_PATCHLEVEL );
 
-    /* wide mpfr_t variables if needed */
+    printf("MPFR library: %-12s\n", mpfr_get_version ());
+    printf("MPFR header : %s (based on %d.%d.%d)\n",
+            MPFR_VERSION_STRING,
+            MPFR_VERSION_MAJOR,
+            MPFR_VERSION_MINOR,
+            MPFR_VERSION_PATCHLEVEL);
+
+    if (mpfr_buildopt_tls_p()!=0)
+        printf("            : compiled as thread safe using TLS\n");
+
+    if (mpfr_buildopt_float128_p()!=0) 
+        printf("            : __float128 support enabled\n");
+
+    if (mpfr_buildopt_decimal_p()!=0)
+        printf("            : decimal float support enabled\n");
+
+    if (mpfr_buildopt_gmpinternals_p()!=0)
+        printf("            : compiled with GMP internals\n");
+
+    if (mpfr_buildopt_sharedcache_p()!=0)
+        printf("            : threads share cache per MPFR const\n");
+
+    printf("            : sizeof(mpfr_prec_t) = %i\n",
+                                      sizeof(mpfr_prec_t));
+
+    printf("MPFR thresholds file used at compile time : %s\n\n",
+                                      mpfr_buildopt_tune_case ());
+
+
+    mpfr_t a, b, t0, t1, t2, t3, t4, t5, t6,
+                 t7, t8, t9, t10, t11, t12,
+                 final;
+
+    /* wide mpfr_t variables if needed
     mpfr_wt tw0, tw1, tw2, tw3, tw4, tw5, tw6;
+    *
+    * * We may need these later .... not now 
+    */
 
     delta_bit_prec = 32;
 
+    mpfr_prec_t actual_prec; /* see _MPFR_PREC_FORMAT */
+    int inex; /* mpfr retval */
+    int debug = 0;
+
+    /* check if a bit precision parameter was on the command line */
+    if (argc>1){
+        errno = 0; /* To distinguish success/failure after call */
+        str = argv[1];
+        bit_prec = strtol(str, &endptr, 10);
+
+        if ( ( (errno == ERANGE)
+               &&
+               ( (bit_prec == LONG_MAX)
+                 || 
+                 (bit_prec == LONG_MIN)
+               ) )
+             || (errno != 0 && bit_prec == 0)) {
+            perror("FAIL : strtol could not parse bit precision.");
+            exit(EXIT_FAILURE);
+        }
+
+        if (endptr == str) {
+            fprintf(stderr, "No bit precision seen.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (*endptr != '\0'){ /* Not necessarily an error... */
+            fprintf(stderr,"INFO : Further characters after bit");
+            fprintf(stderr," precision ignored : %s\n", endptr);
+        }
+
+        if (bit_prec>1023){
+            bit_prec = 1024;
+            printf("\nWARNING : limit precision to 1024 bits.\n");
+        }
+
+        if (bit_prec<128) {
+            bit_prec = 128;
+            printf("\nWARNING : precision minimum is 128 bits.\n");
+        }
+
+        if ( bit_prec%32 ) {
+            /* note that this always adjusts upwards and not
+             * to the nearest 32 bit boundary */
+            bit_prec = bit_prec + 32 - bit_prec%32;
+        }
+
+    } else {
+        bit_prec = PREC;
+    }
+    delta_bit_prec = bit_prec / 2;
     mpfr_set_default_prec((mpfr_prec_t)bit_prec);
-    mpfr_inits2( prec, t0, t1, t2, t3, t4, t5, t6, (mpfr_ptr*) 0 );
-    /* we may not need these .. in yer dreams 
-    mpfr_inits2( prec, tw0, tw1, tw2, tw3, tw4, tw5, tw6, (mpfr_ptr*) 0 );
-    */
+    actual_prec=mpfr_get_default_prec();
+    if ( actual_prec == bit_prec ) {
+        printf("Requested %i bits of precision accepted.\n", bit_prec);
+    } else {
+        printf("WARN : we shall use %i bits of precision.\n", bit_prec);
+    }
+
+    mpfr_inits2( bit_prec, a, b, t0, t1, t2, t3, t4, t5, t6,
+                                 t7, t8, t9, t10, t11, t12,
+                                 final, (mpfr_ptr*) 0 );
+
+    /* TODO check the MPFR return values and exceptions */
+
+    /* init a */
+    mpfr_set_ui(a, 77617, MPFR_RNDN);
+    mpfr_printf ("\n\n        a = %.Re\n", a);
+
+    /* init b */
+    mpfr_set_ui(b, 33096, MPFR_RNDN);
+    mpfr_printf ("        b = %.Re\n", b);
+
+    /* compute b^6 */
+    mpfr_pow_ui(t0, b, 6, MPFR_RNDN);
+    mpfr_printf ("       t0 = %.Re\n", t0);
+
+    /* compute 333.75 * t0 = 333.75 * b^6 */
+    mpfr_mul_d(t0, t0, 333.75, MPFR_RNDN);
+    mpfr_printf ("       t0 = %.Re\n", t0);
+
+    /* compute a^2 */
+    mpfr_pow_ui(t1, a, 2, MPFR_RNDN);
+    mpfr_printf ("       t1 = %.Re\n", t1);
+
+    /* compute b^2 */
+    mpfr_pow_ui(t2, b, 2, MPFR_RNDN);
+    mpfr_printf ("       t2 = %.Re\n", t2);
+
+    /* compute a^2  *   b^2   ==   t1 * t2 */
+    mpfr_mul(t3, t1, t2, MPFR_RNDN);
+    mpfr_printf ("       t3 = %.Re\n", t3);
+
+    /* compute 11 * a^2 * b^2 == 11 * t3 */
+    mpfr_mul_ui(t3, t3, 11, MPFR_RNDN);
+    mpfr_printf ("       t3 = %.Re\n", t3);
+
+    /* compute b^6 */
+    mpfr_pow_ui(t4, t2, 3, MPFR_RNDN);
+    mpfr_printf ("       t4 = %.Re\n", t4);
+
+    /* compute b^4 */
+    mpfr_pow_ui(t5, t2, 2, MPFR_RNDN);
+    mpfr_printf ("       t5 = %.Re\n", t5);
+
+    /* compute 121 * b^4 */
+    mpfr_mul_ui(t6, t5, 121, MPFR_RNDN);
+    mpfr_printf ("       t6 = %.Re\n", t6);
+
+    /* compute 11 * a^2 * b^2  -  b^6 */
+    /* -1314174461784456350457997632 */
+    mpfr_sub(t7, t3, t4, MPFR_RNDN);
+    mpfr_printf ("       t7 = %.Re\n", t7);
+
+    /* now subtract 121 * b^4 from that */
+    /* -1314174606957974558362483008 */
+    mpfr_sub(t8, t7, t6, MPFR_RNDN);
+    mpfr_printf ("       t8 = %.Re\n", t8);
+
+    /* now subtract 2 from that */
+    /* -1314174606957974558362483010 */
+    mpfr_sub_ui(t8, t8, 2, MPFR_RNDN);
+    mpfr_printf ("       t8 = %.Re\n", t8);
+
+    /* multiply a^2 * ( 11 * a^2 * b^2 - b^6 - 121 * b^4 - 2 ) */
+    /*  -7917111779274712207494296632228773890  */
+    mpfr_mul(t9, t1, t8, MPFR_RNDN);
+    mpfr_printf ("       t9 = %.Re\n", t9);
+
+    /* compute b^8 from the previous b^4 */
+    /* 1439474789212538429291115400277262336 */
+    mpfr_pow_ui(t10, t5, 2, MPFR_RNDN);
+    mpfr_printf ("b^8 = t10 = %.Re\n", t10);
+    
+    /* now multiply 5.5 */
+    /* 7917111340668961361101134701524942848 */
+    mpfr_mul_d(t10, t10, 5.5, MPFR_RNDN);
+    mpfr_printf ("5.5*      = %.Re\n", t10);
+
+    /* compute  a / b */
+    mpfr_div(t11, a, b, MPFR_RNDN);
+    mpfr_printf ("a/b = t11 = %.Re\n", t11);
+
+    /* now divide that by 2 */
+    mpfr_div_ui(t12, t11, 2, MPFR_RNDN);
+    mpfr_printf ("a/(b*2)   = %.Re\n", t12);
+
+    /* add in the components to a final accumulator */
+    mpfr_add(final, t0, t9, MPFR_RNDN);
+    mpfr_printf ("          = %.Re\n", final);
+    mpfr_add(final, final, t10, MPFR_RNDN);
+    mpfr_printf ("          = %.Re\n", final);
+    mpfr_add(final, final, t12, MPFR_RNDN);
+    mpfr_printf ("\n\n    final = %.Re\n", final);
+
+    printf ("  correct = -8.2739605994682136814116509547981629199903311578438481991777e-01\n");
 
 
-
-
-
-
-
-
-
-
-
-
+    mpfr_clears( a, b, 
+                 t0, t1, t2, t3, t4, t5, t6,
+                 t7, t8, t9, t10, t11, t12,
+                 final, (mpfr_ptr*) 0 );
 
 
     return EXIT_SUCCESS;
 
 }
 
+int mpfr_check_flags( int mpfr_status, int debug_flag )
+{
+    /* some mpfr call resulted in a mpfr_status value
+     * so lets check the flags and determine if the
+     * status should be cleared to zero or not */
+
+    int mpfr_underflow_flag, mpfr_overflow_flag, mpfr_divby0_flag,
+        mpfr_nanflag_flag, mpfr_inexflag_flag, mpfr_erangeflag_flag;
+
+    mpfr_underflow_flag = mpfr_underflow_p();
+    if ( mpfr_underflow_flag != 0 ){
+        printf ("INFO : mpfr_underflow_flag is set.\n" );
+        /* treat underflow as an error situation */
+        mpfr_status = -1;
+    }
+
+    mpfr_overflow_flag = mpfr_overflow_p();
+    if ( mpfr_overflow_flag != 0 ){
+        printf ("INFO : mpfr_overflow_flag is set.\n" );
+        /* treat overflow as an error situation */
+        mpfr_status = -1;
+    }
+
+    mpfr_divby0_flag = mpfr_divby0_p();
+    if ( mpfr_divby0_flag != 0 ){
+        printf ("INFO : mpfr_divby0_flag is set.\n" );
+        /* divide by zero is definately an error */
+        mpfr_status = -1;
+    }
+
+    mpfr_nanflag_flag = mpfr_nanflag_p();
+    if ( mpfr_nanflag_flag != 0 ){
+        printf ("INFO : mpfr_nanflag_flag is set.\n" );
+        /* a NaN may not be an error condition */
+        mpfr_status = 0;
+    }
+
+    mpfr_inexflag_flag = mpfr_inexflag_p();
+    if ( mpfr_inexflag_flag != 0 ){
+        /* a computation was not exact */
+        if (debug_flag) fprintf (stderr,"WARN : mpfr_inexflag_flag is set.\n");
+        mpfr_status = -1;
+    }
+
+    mpfr_erangeflag_flag = mpfr_erangeflag_p();
+    if ( mpfr_erangeflag_flag != 0 ){
+        printf ("INFO : mpfr_erangeflag_flag is set.\n" );
+        /* treat a range error as a valid error */
+        mpfr_status = -1;
+    }
+
+    return mpfr_status;
+
+}
 
