689b767502ac3ed0f9f72471ed26e02ffa178848
blastwave <dclarke@blastwave.org>
2022-01-11 19:46:20 +0000

    20220111194620 : test message digest code on FreeBSD i686

---
+++ openssl/common.mk
@@ -0,0 +1,17 @@
+CC?=		/usr/bin/cc
+CPPFLAGS=	-D_TS_ERRNO -D_POSIX_PTHREAD_SEMANTICS -D_LARGEFILE64_SOURCE \
+		-D_REENTRANT
+
+LIBS=		-lssl -lcrypto
+
+OBJS=		dgst_test.o
+
+.PHONY: all
+all: dgst_test
+
+dgst_test: $(OBJS)
+	$(CC) -o dgst_test $(OBJS) $(CFLAGS) $(CPPFLAGS) $(LIBS)
+
+.PHONY: clean
+clean:
+	rm -f $(OBJS) dgst_test
