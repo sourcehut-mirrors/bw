24f7986c213789b3ff285a1872cc9a53d417b683
blastwave <dclarke@blastwave.org>
2022-01-11 15:48:41 +0000

    20220111154841 : quick test of OpenSSL EVP_Digest calls

---
+++ openssl/dgst_test.c
@@ -0,0 +1,245 @@
+
+/**********************************************************************
+  LICENSE ISSUES
+  ==============
+
+  The OpenSSL toolkit stays under a double license, i.e. both the
+  conditions of the OpenSSL License and the original SSLeay license
+  apply to the toolkit.  See below for the actual license texts.
+
+  OpenSSL License
+  ---------------
+
+ * ====================================================================
+ * Copyright (c) 1998-2019 The OpenSSL Project.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
+ *
+ * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
+ *    endorse or promote products derived from this software without
+ *    prior written permission. For written permission, please contact
+ *    openssl-core@openssl.org.
+ *
+ * 5. Products derived from this software may not be called "OpenSSL"
+ *    nor may "OpenSSL" appear in their names without prior written
+ *    permission of the OpenSSL Project.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment:
+ *    "This product includes software developed by the OpenSSL Project
+ *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ *
+
+ Original SSLeay License
+ -----------------------
+
+ * Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
+ * All rights reserved.
+ *
+ * This package is an SSL implementation written
+ * by Eric Young (eay@cryptsoft.com).
+ * The implementation was written so as to conform with Netscapes SSL.
+ *
+ * This library is free for commercial and non-commercial use as long as
+ * the following conditions are aheared to.  The following conditions
+ * apply to all code found in this distribution, be it the RC4, RSA,
+ * lhash, DES, etc., code; not just the SSL code.  The SSL documentation
+ * included with this distribution is covered by the same copyright terms
+ * except that the holder is Tim Hudson (tjh@cryptsoft.com).
+ *
+ * Copyright remains Eric Young's, and as such any Copyright notices in
+ * the code are not to be removed.
+ * If this package is used in a product, Eric Young should be given attribution
+ * as the author of the parts of the library used.
+ * This can be in the form of a textual message at program startup or
+ * in documentation (online or textual) provided with the package.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    "This product includes cryptographic software written by
+ *     Eric Young (eay@cryptsoft.com)"
+ *    The word 'cryptographic' can be left out if the rouines from the library
+ *    being used are not cryptographic related :-).
+ * 4. If you include any Windows specific code (or a derivative thereof) from
+ *    the apps directory (application code) you must include an acknowledgement:
+ *    "This product includes software written by Tim Hudson (tjh@cryptsoft.com)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY ERIC YOUNG ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * The licence and distribution terms for any publically available version or
+ * derivative of this code cannot be changed.  i.e. this code cannot simply be
+ * copied and put under another distribution licence
+ * [including the GNU Public Licence.]
+ */
+
+
+/* dgst_test.c  Trivial sample code to call into the OpenSSL libraries
+ *              wherein we may perform hash functions on sample test
+ *              vector data.
+ *
+ *              This code is modified and will likely continue to be
+ *              hacked at by me.
+ *
+ * Copyright (C) Dennis Clarke 2022
+ *
+ * For information regarding the OpenSSL licenses please see :
+ *
+ *         https://www.openssl.org/source/license.html
+ */
+
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+#define _XOPEN_SOURCE 600
+
+     Likely will nto work with the OpenSSL sources which are C89
+
+ *********************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <openssl/evp.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    EVP_MD_CTX *mdctx;
+    const   EVP_MD *md;
+
+    /* please see the reference test vector discussion at
+     * https://datatracker.ietf.org/doc/html/rfc7693#appendix-A
+     *
+     * Here we see the input "abc" should produce the output
+     * for BLAKE2b-512 algorithm :
+     *
+     *
+     *  BA 80 A5 3F 98 1C 4D 0D 6A 27 97 B6 9F 12 F6 E9
+     *  4C 21 2F 14 68 5A C4 B7 4B 12 BB 6F DB FF A2 D1
+     *  7D 87 C5 39 2A AB 79 2D C2 52 D5 DE 45 33 CC 95
+     *  18 D3 8A A8 DB F1 92 5A B9 23 86 ED D4 00 99 23
+     *
+     */
+    char    mess0[] = "abc";
+
+/* we don't need these at the moment
+    char    mess1[] = "Test Message\n";
+    char    mess2[] = "Hello World\n";
+*/
+
+    unsigned char   md_value[EVP_MAX_MD_SIZE];
+    unsigned int    md_len, i;
+
+    if (argv[1] == NULL) {
+        fprintf(stderr,"Usage: %s digest_algorithm_name\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    md = EVP_get_digestbyname(argv[1]);
+    if (md == NULL) {
+        /*
+         * Please see OpenSSL manual for EVP_DigestInit
+         * and also see EVP_MD_meth_new.
+         *
+         * Digest algorithms may be listed thus :
+         *
+         * $ openssl list --digest-commands
+         * blake2b512       blake2s256       gost             md4
+         * md5              mdc2             rmd160           sha1
+         * sha224           sha256           sha3-224         sha3-256
+         * sha3-384         sha3-512         sha384           sha512
+         * sha512-224       sha512-256       shake128         shake256
+         * sm3
+         *
+         */
+        fprintf(stderr,"FAIL : Unknown message digest %s\n", argv[1]);
+  
+        fprintf(stderr,"    : Please use one of the following :\n");
+        fprintf(stderr,"    :   md4, md5, mdc2, rmd160, gost, sha1,\n");
+        fprintf(stderr,"    :   sm3, shake128, shake256, sha224,\n");
+        fprintf(stderr,"    :   sha256, sha384, sha512,\n");
+        fprintf(stderr,"    :   sha512-224, sha512-256, sha3-224,\n");
+        fprintf(stderr,"    :   sha3-256, sha3-384, sha3-512,\n");
+        fprintf(stderr,"    :   blake2s256, blake2b512.\n");
+
+        return EXIT_FAILURE;
+    }
+
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, mess0, strlen(mess0));
+/*
+    EVP_DigestUpdate(mdctx, mess1, strlen(mess1));
+    EVP_DigestUpdate(mdctx, mess2, strlen(mess2));
+*/
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+    EVP_MD_CTX_free(mdctx);
+
+    printf("Digest is: ");
+    for (i = 0; i < md_len; i++) {
+        printf("%02X ", md_value[i]);
+    }
+    printf("\n");
+
+    return EXIT_SUCCESS;
+
+}
+
