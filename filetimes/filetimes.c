380a717f00e4f25a6524fb361824626ffc256ecd
blastwave <dclarke@blastwave.org>
2022-01-31 17:40:24 +0000

    20220131174024 : attempt to clean up filetimes a bit

--- filetimes/filetimes.c
+++ filetimes/filetimes.c
@@ -1,4 +1,24 @@
 
+/*
+ * filetimes.c report the various essential UNIX timestamps for a file
+ * Copyright (C) Dennis Clarke 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
 /*********************************************************************
  * The Open Group Base Specifications Issue 6
  * IEEE Std 1003.1, 2004 Edition
@@ -9,8 +29,8 @@
  *    functionality described in The _POSIX_C_SOURCE Feature Test
  *    Macro and in addition to enable the XSI extension.
  *
-#define _XOPEN_SOURCE 600
  *********************************************************************/
+#define _XOPEN_SOURCE 600
 
 #include <locale.h>
 #include <stdio.h>
@@ -42,6 +62,7 @@ int main(int argc, char **argv)
     struct timespec start_tv;
 
     if ( argc < 2 ) {
+        fprintf(stderr,"FAIL : provide a filename or pathname\n");
         errno = EINVAL;
         perror("FAIL ");
         return EXIT_FAILURE;
@@ -59,6 +80,7 @@ int main(int argc, char **argv)
 
     c_time_string = ctime( &start_tv.tv_sec );
 
+    errno = 0;
     status = stat(argv[1], &status_buffer);
     if ( status == 0 ) {
         fprintf (stderr,"\nINFO : current time is %s", c_time_string );
