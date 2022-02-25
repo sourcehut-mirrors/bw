80fb0110ab4fe269c7688ef441f199fc7dc85266
Dennis Clarke <dclarke@blastwave.org>
2022-02-25 02:25:54 +0000

    20220225022554 : note that NVidia CUDA mem transfers are slow

--- nvidia_cuda/include/dat.h
+++ nvidia_cuda/include/dat.h
@@ -1,5 +1,6 @@
 
 /* some possible values for modern GPU tech 
+ * 67108864     64M
  * 134217728    128M
  * 268435456    256M
  * 402653184    384M
@@ -9,9 +10,13 @@
  *
  * minimal low level entry point at 16M is 
  *            16777216   elements
+ *
+ * NOTE :   Pay attention to the fact that the 
+ *          64-bit elements are 8 bytes each and
+ *          the 32-bit elements are 4 bytes each.
  */
 
-#define NUM_ELEMENTS 268435456
+#define NUM_ELEMENTS 134217728
 #define THREADS_PER_BLOCK 1024
 #define EPSILON 1e-12
 
