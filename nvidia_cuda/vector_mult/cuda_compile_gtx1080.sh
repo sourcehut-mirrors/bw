f941ded4937ef2afde3054e3fbc11cc5b3186fee
Dennis Clarke <dclarke@blastwave.org>
2022-03-20 00:13:53 +0000

    20220320001353 : write a update to readme for GTX1080 etc and compile script

---
+++ nvidia_cuda/vector_mult/cuda_compile_gtx1080.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PATH=/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/cuda-11.4/bin:/opt/schily/bin
+
+NVCC=`(command -v nvcc)` ; export NVCC
+
+rm -f vmult.o vmult > /dev/null 2>&1
+
+${NVCC} -ccbin g++ -I../include -m64 -gencode arch=compute_61,code=sm_61 -Wno-deprecated-gpu-targets --ftz=false --prec-div=true --prec-sqrt=true -fmad=false -c -o vmultd.o vmultd.cu 
+
+${NVCC} -ccbin g++ -m64 -gencode arch=compute_61,code=sm_61 -Wno-deprecated-gpu-targets --ftz=false --prec-div=true --prec-sqrt=true -fmad=false -o vmultd vmultd.o -lgomp
+
+/usr/bin/printf "\n------- code will run in five seconds .. or stop me!\n\n"
+
+ls -lapb vmult*
+
+sleep 5 
+
+NVPROF=`( command -v nvprof )`; export NVPROF
+
+${NVPROF} ./vmultd
+
