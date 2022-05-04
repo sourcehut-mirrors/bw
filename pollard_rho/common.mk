1af71c98569e93a951b433a169bc3063c0bbedf2
blastwave <dclarke@blastwave.org>
2022-05-04 11:31:26 +0000

    20220504113126 : verify the index offset computation

---
+++ pollard_rho/common.mk
@@ -0,0 +1,19 @@
+
+CPPFLAGS= -D_POSIX_PTHREAD_SEMANTICS -D_LARGEFILE64_SOURCE \
+	-D_XOPEN_SOURCE=600
+
+LDIR=/usr/local/lib
+IDIR=/usr/local/include
+LIBS=-lgmp -lmpfr
+
+SRCS = pr_mpfr_quiet.c ../sysinfo/sysinfo.c
+OBJS = pr_mpfr_quiet.o ../sysinfo/sysinfo.o
+
+.c.o:
+	$(CC) -c -o $@ $< $(CFLAGS) -I$(IDIR) $(CPPFLAGS)
+
+pr_mpfr_quiet: $(OBJS)
+	$(CC) -o $@ $(OBJS) $(CFLAGS) $(CPPFLAGS) -L$(LDIR) $(LIBS)
+
+clean:
+	rm -f $(OBJS) pr_mpfr_quiet
