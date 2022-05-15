8fc8466732898f9d243e1d62ab5616c4627e3764
blastwave <dclarke@blastwave.org>
2022-05-15 05:40:29 +0000

    20220515054029 : try the openat calls which require C11

--- in_progress/data_dump/timedump.c
+++ in_progress/data_dump/timedump.c
@@ -22,22 +22,10 @@
  * https://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-/*********************************************************************
- * The Open Group Base Specifications Issue 6
- * IEEE Std 1003.1, 2004 Edition
- *
- *    An XSI-conforming application should ensure that the feature
- *    test macro _XOPEN_SOURCE is defined with the value 600 before
- *    inclusion of any header. This is needed to enable the
- *    functionality described in The _POSIX_C_SOURCE Feature Test
- *    Macro and in addition to enable the XSI extension.
- *
- *********************************************************************/
-#define _XOPEN_SOURCE 600
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <inttypes.h>
 #include <time.h>
 #include <locale.h>
@@ -58,36 +46,56 @@ int main ( int argc, char **argv) {
 
     FILE *fp;
     struct stat status_buffer;
-    size_t num_written;
+    size_t num_written, filename_len;
+    struct tm *ptm;
 
     time_t time_now;
-    char timestamp_filename[32];
+    char *timestamp_filename;
+    char timestamp[32];
+    char *err_status;
 
     double c_r = REAL_COORD;
     double c_j = IMAG_COORD;
     int bail_out = BAIL_OUT;
     int magnify = MAGNIFY;
     int num_elements = NUM_ELEMENTS;
+    int status;
+    char *tmpdir;
 
     setlocale (LC_ALL, "POSIX");
 
-    int status = setenv("TZ", "GMT0", 1);
+    status = setenv("TZ", "GMT0", 1);
     if ( status < 0 ) {
         fprintf (stderr,"FAIL : can not set timezone TZ = GMT0\n");
+        return EXIT_FAILURE;
+    }
+
+    tmpdir = getenv("TMPDIR");
+    if ( tmpdir == NULL ) {
+        printf("No TMPDIR");
         return EXIT_FAILURE;
     }
 
     sysinfo(VERBOSE);
 
     time(&time_now);
-    struct tm *ptm = gmtime(&time_now);
+    ptm = gmtime(&time_now);
+    filename_len = strftime(timestamp, 32, "%Y%m%d%H%M%S", ptm);
+    time(&time_now);
 
-    setlocale (LC_ALL, "POSIX");
+    timestamp_filename = calloc(_POSIX_PATH_MAX,sizeof(unsigned char));
+    /* TODO check the calloc return value */
 
-    size_t filename_len = strftime(timestamp_filename, 32, "%Y%m%d%H%M%S", ptm);
+    int directory_fd = openat(AT_FDCWD, tmpdir, O_RDONLY | O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW);
 
-    printf("DBUG : filename len = %lu\n", filename_len);
-    printf("     : filename     = \"%s\"\n", timestamp_filename);
+    int file_fd = openat(directory_fd, timestamp, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+    fp = fdopen(file_fd, "wb");
+
+    /*
+    err_status = strcat(timestamp_filename, tmpdir);
+    err_status = strcat(timestamp_filename, "/");
+    err_status = strcat(timestamp_filename, timestamp);
 
     status = stat(timestamp_filename, &status_buffer);
     if ( status == 0 ) {
@@ -97,6 +105,8 @@ int main ( int argc, char **argv) {
 
     errno = 0;
     fp = fopen(timestamp_filename, "wb");
+    */
+
     if ( fp == NULL ) {
         perror("FAIL ");
         return EXIT_FAILURE;
@@ -115,34 +125,39 @@ int main ( int argc, char **argv) {
     /* strictly speaking this is not a wise way to do this */
     uint8_t endian_flag = (*(uint8_t*)&end_check == 1) ? 0 : 16;
 
+    fprintf (stderr,"INFO : file %s/%s dump begins.\n",tmpdir,timestamp);
     /* if the machine is big endian we get endian_flag = 0x10 */
-
-    num_written = fwrite( &endian_flag, sizeof(uint8_t), 1, fp);
+    num_written = fwrite(&endian_flag, sizeof(uint8_t), 1, fp);
     printf("DBUG : %2lu byte uint8_t endian_flag   num_written = %lu\n",
             sizeof(uint8_t), num_written);
 
-    num_written = fwrite( &c_r, sizeof(double), 1, fp);
-    printf("DBUG : %2lu byte double  c_r           num_written = %lu\n",
+    num_written = fwrite(&num_elements, sizeof(uint32_t), 1, fp);
+    printf("     : %2lu byte uint32_t num_elements num_written = %lu\n",
+            sizeof(uint32_t), num_written);
+    printf("     : num_elements = %8i\n",num_elements);
+
+    num_written = fwrite(&bail_out, sizeof(uint32_t), 1, fp);
+    printf("     : %2lu byte uint32_t bail_out     num_written = %lu\n",
+            sizeof(uint32_t), num_written);
+    printf("     : bail_out = %8i\n",bail_out);
+
+    num_written = fwrite(&magnify, sizeof(double), 1, fp);
+    printf("     : %2lu byte double magnify        num_written = %lu\n",
             sizeof(double), num_written);
+    printf("     :        magnify = %-+26.20e\n", magnify);
 
-    num_written = fwrite( &c_j, sizeof(double), 1, fp);
-    printf("     : %2lu byte double  c_j           num_written = %lu\n",
+    num_written = fwrite(&c_r, sizeof(double), 1, fp);
+    printf("DBUG : %2lu byte double c_r num_written = %lu\n",
             sizeof(double), num_written);
+    printf("     : c_r = %-+26.20e\n",c_r);
 
-    num_written = fwrite( &bail_out, sizeof(int), 1, fp);
-    printf("     : %2lu byte integer bail_out      num_written = %lu\n",
-            sizeof(int), num_written);
-
-    num_written = fwrite( &magnify, sizeof(int), 1, fp);
-    printf("     : %2lu byte integer magnify       num_written = %lu\n",
-            sizeof(int), num_written);
-
-    num_written = fwrite( &num_elements, sizeof(int), 1, fp);
-    printf("     : %2lu byte integer num_elements  num_written = %lu\n",
-            sizeof(int), num_written);
-
+    num_written = fwrite(&c_j, sizeof(double), 1, fp);
+    printf("     : %2lu byte double c_j num_written = %lu\n",
+            sizeof(double), num_written);
+    printf("     : c_j = %-+26.20e\n",c_j);
 
     fclose(fp);
+    fprintf (stderr,"INFO : file %s closed.\n",timestamp_filename);
 
     return EXIT_SUCCESS;
 
