81c4e6b02bd17ae0dcb2db51b176445878ba90e1
Dennis Clarke <dclarke@blastwave.org>
2022-06-12 04:53:17 +0000

    20220612045317 : test big endian to little endian swap

--- in_progress/data_dump/timedump.c
+++ in_progress/data_dump/timedump.c
@@ -39,6 +39,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iso646.h>
 #include <limits.h>
 #include <inttypes.h>
 #include <time.h>
@@ -55,6 +56,49 @@
 #define IMAG_COORD 0.205251797480741515756
 
 #define VERBOSE 1
+
+/* swap bytes around to change endian */
+#define htonll(x) (((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+
+
+uint64_t rot8(uint64_t x)
+{
+
+    uint64_t result, b[8] = {0,0,0,0,0,0,0,0};
+
+    b[0] = ( ( x & 0xffULL ) << 56 );
+    b[1] = ( ( x & 0xff00ULL ) << 40 );
+    b[2] = ( ( x & 0xff0000ULL ) << 24 );
+    b[3] = ( ( x & 0xff000000ULL ) << 8 );
+    b[4] = ( ( x & 0xff00000000ULL ) >> 8 );
+    b[5] = ( ( x & 0xff0000000000ULL ) >> 24 );
+    b[6] = ( ( x & 0xff000000000000ULL ) >> 40 );
+    b[7] = ( x >> 56 );
+
+    result = b[0] | b[1] | b[2] | b[3] | b[4] | b[5] | b[6] | b[7];
+
+    return result;
+
+}
+
+
+uint32_t rot4(uint32_t x)
+{
+    uint32_t result, b[4] = {0,0,0,0};
+
+    b[0] = ( ( x & 0xff ) << 24 );
+    b[1] = ( ( x & 0xff00 ) << 8 );
+    b[2] = ( ( x & 0xff0000 ) >> 8 );
+    b[3] = ( ( x & 0xff000000 ) >> 24 );
+
+    result = b[0] | b[1] | b[2] | b[3];
+
+    return result;
+
+}
+
+
+
 int sysinfo(int verbose);
 
 int main ( int argc, char **argv) {
@@ -72,10 +116,13 @@ int main ( int argc, char **argv) {
     double c_r = REAL_COORD;
     double c_j = IMAG_COORD;
     int bail_out = BAIL_OUT;
-    int magnify = MAGNIFY;
+    double magnify = MAGNIFY;
     int num_elements = NUM_ELEMENTS;
     int status;
     char *tmpdir;
+
+    uint64_t rotated64;
+    uint32_t rotated32;
 
     setlocale (LC_ALL, "POSIX");
 
@@ -99,7 +146,18 @@ int main ( int argc, char **argv) {
     time(&time_now);
 
     timestamp_filename = calloc(_POSIX_PATH_MAX,sizeof(unsigned char));
-    /* TODO check the calloc return value */
+    if ( timestamp_filename == NULL ) {
+        /* really? possible ENOMEM? */
+        if ( errno == ENOMEM ) {
+            fprintf(stderr,"FAIL : calloc returns ENOMEM at %s:%d\n",
+                    __FILE__, __LINE__ );
+        } else {
+            fprintf(stderr,"FAIL : calloc fails at %s:%d\n",
+                    __FILE__, __LINE__ );
+        }
+        perror("FAIL ");
+        return EXIT_FAILURE;
+    }
 
     err_status = strcat(timestamp_filename, tmpdir);
 
@@ -142,33 +200,68 @@ int main ( int argc, char **argv) {
     printf("DBUG : %2lu byte uint8_t endian_flag   num_written = %lu\n",
             sizeof(uint8_t), num_written);
 
-    num_written = fwrite(&num_elements, sizeof(uint32_t), 1, fp);
+
+    /* if this machine is big endian then we swap around the bytes
+     * to give us little endian output in the file */
+    if ( endian_flag ) {
+        /* a test integer which is bytes 0x12345678 */
+        /* int test_int = 305419896; */
+        /* rotated32 = rot4(test_int); */
+        rotated32 = rot4(num_elements);
+        num_written = fwrite(&rotated32, sizeof(uint32_t), 1, fp);
+    } else {
+        num_written = fwrite(&num_elements, sizeof(uint32_t), 1, fp);
+    }
     printf("     : %2lu byte uint32_t num_elements num_written = %lu\n",
-            sizeof(uint32_t), num_written);
+                sizeof(uint32_t), num_written);
     printf("     : num_elements = %8i\n",num_elements);
 
-    num_written = fwrite(&bail_out, sizeof(uint32_t), 1, fp);
+    if ( endian_flag ) {
+        rotated32 = rot4(bail_out);
+        num_written = fwrite(&rotated32, sizeof(uint32_t), 1, fp);
+    } else {
+        num_written = fwrite(&bail_out, sizeof(uint32_t), 1, fp);
+    }
     printf("     : %2lu byte uint32_t bail_out     num_written = %lu\n",
             sizeof(uint32_t), num_written);
     printf("     : bail_out = %8i\n",bail_out);
 
-    num_written = fwrite(&magnify, sizeof(double), 1, fp);
+    /* need to swap around bytes of the 8-byte floating point double */
+    if ( endian_flag ) {
+        rotated64 = rot8(*((uint64_t *)&magnify));
+        num_written = fwrite(&rotated64, sizeof(uint64_t), 1, fp);
+    } else {
+        num_written = fwrite(&magnify, sizeof(double), 1, fp);
+    }
     printf("     : %2lu byte double magnify        num_written = %lu\n",
             sizeof(double), num_written);
     printf("     :        magnify = %-+26.20e\n", magnify);
 
-    num_written = fwrite(&c_r, sizeof(double), 1, fp);
+    if ( endian_flag ) {
+        rotated64 = rot8(*((uint64_t *)&c_r));
+        num_written = fwrite(&rotated64, sizeof(uint64_t), 1, fp);
+    } else {
+        num_written = fwrite(&c_r, sizeof(double), 1, fp);
+    }
     printf("DBUG : %2lu byte double c_r num_written = %lu\n",
             sizeof(double), num_written);
     printf("     : c_r = %-+26.20e\n",c_r);
 
-    num_written = fwrite(&c_j, sizeof(double), 1, fp);
+    if ( endian_flag ) {
+        rotated64 = rot8(*((uint64_t *)&c_j));
+        num_written = fwrite(&rotated64, sizeof(uint64_t), 1, fp);
+    } else {
+        num_written = fwrite(&c_j, sizeof(double), 1, fp);
+    }
     printf("     : %2lu byte double c_j num_written = %lu\n",
             sizeof(double), num_written);
     printf("     : c_j = %-+26.20e\n",c_j);
 
     fclose(fp);
     fprintf (stderr,"INFO : file %s closed.\n",timestamp_filename);
+
+    free(timestamp_filename);
+    timestamp_filename = NULL;
 
     return EXIT_SUCCESS;
 
