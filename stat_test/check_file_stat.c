a2a781db4166c6d20c828fcfe656b5689cf17a50
blastwave <dclarke@blastwave.org>
2022-08-19 14:58:33 +0000

    20220819145833 : briefly look at stat return status_buffer.st_flags

--- stat_test/check_file_stat.c
+++ stat_test/check_file_stat.c
@@ -12,6 +12,8 @@
 #define VERBOSE 1
 int sysinfo(int verbose);
 
+int file_stat_err( int file_errno );
+
 int main(int argc, char **argv) 
 {
 
@@ -22,6 +24,7 @@ int main(int argc, char **argv)
     sysinfo(VERBOSE);
 
     if (argc>1) {
+        errno = 0;
         status = stat(argv[1], &status_buffer);
     } else {
         fprintf ( stderr, "FAIL : give a filename\n");
@@ -30,71 +33,10 @@ int main(int argc, char **argv)
 
     /* check that we have a valid status and if we do not
      * then check for a pile of possible errors : 
-     *
      */
 
     if ( status < 0 ) {
-        /* check for a pile of things that could have gone
-         * wrong */
-        switch(errno) {
-            case EFAULT :
-                fprintf ( stderr, "ERROR : EFAULT\n" );
-                break;
-            case ENOENT :
-                fprintf ( stderr, "ERROR : ENOENT\n" );
-                break;
-            case EBADF :
-                fprintf ( stderr, "ERROR : EBADF\n" );
-                break;
-            default :
-                fprintf ( stderr, "ERROR : something bad happened.\n" );
-        }
-        perror("ERROR ");
-
-
-        /* all manner of bad stuff can happen
-         *
-       EACCES Search  permission  is  denied for one of the directories in the
-              path prefix of pathname.  (See also path_resolution(7).)
-
-       EBADF  fd is not a valid open file descriptor.
-
-       EFAULT Bad address.
-
-       ELOOP  Too many symbolic links encountered while traversing the path.
-
-       ENAMETOOLONG
-              pathname is too long.
-
-       ENOENT A component of pathname does not exist, or pathname is an  empty
-              string and AT_EMPTY_PATH was not specified in flags.
-
-       ENOMEM Out of memory (i.e., kernel memory).
-
-       ENOTDIR
-              A component of the path prefix of pathname is not a directory.
-
-       EOVERFLOW
-              pathname  or  fd  refers  to a file whose size, inode number, or
-              number of blocks cannot be  represented  in,  respectively,  the
-              types off_t, ino_t, or blkcnt_t.  This error can occur when, for
-              example, an application compiled on a  32-bit  platform  without
-              -D_FILE_OFFSET_BITS=64 calls stat() on a file whose size exceeds
-              (1<<31)-1 bytes.
-
-       The following additional errors can occur for fstatat():
-
-       EBADF  dirfd is not a valid file descriptor.
-
-       EINVAL Invalid flag specified in flags.
-
-       ENOTDIR
-              pathname is relative and dirfd is a file descriptor referring to
-              a file other than a directory.
-
-
-     */
-
+        status = file_stat_err(errno);
     } else {
 
         /* print the file creation time */
---
+++ stat_test/common.mk
@@ -0,0 +1,26 @@
+
+CC?=	/usr/bin/cc
+
+CPPFLAGS=	-D_POSIX_PTHREAD_SEMANTICS -D_LARGEFILE64_SOURCE \
+			-D_XOPEN_SOURCE=600
+
+LIBS=		-lm
+
+LDIR?=		/usr/local/lib
+IDIR?=		/usr/local/include
+
+OBJS=		./file_stat_err.o \
+		../sysinfo/sysinfo.o
+
+.PHONY: all
+all: check_file_stat
+
+.c.o:
+	$(CC) -c -o $@ $< $(CFLAGS) $(CPPFLAGS) -I$(IDIR)
+
+check_file_stat: check_file_stat.o $(OBJS)
+	$(CC) -o check_file_stat check_file_stat.o $(OBJS) $(CFLAGS) $(CPPFLAGS) -L$(LDIR) $(LIBS)
+
+.PHONY: clean
+clean:
+	rm -f $(OBJS) check_file_stat.o check_file_stat
