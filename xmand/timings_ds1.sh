8f1459c5c43e80f718106a6149c89087ef9d2d8a
Dennis Clarke <dclarke@blastwave.org>
2022-08-26 15:18:33 +0000

    20220826151833 : treat nsec timings as strings in awk script

--- xmand/timings_ds1.sh
+++ xmand/timings_ds1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 grep 'nsec' output/readme.dataset1* | cut -f2 -d\:  | grep -v 'INFO' | cut -f2 -d\=  | sort -n | awk 'BEGIN{print"#!/bin/sh"}{print "/usr/bin/printf \047" $1 "    \047\ngrep \047" $1 "\047 output/readme.dataset1* | cut -f1 -d\: "}' > $TMPDIR/wat.sh; chmod 0755 $TMPDIR/wat.sh
-$TMPDIR/wat.sh | awk '{ printf("%14i    %s\n", $1, $2)}'
+$TMPDIR/wat.sh | sed -e 's/output\/readme\.dataset1_//' | awk '{ printf("%14s    %s\n", $1, $2)}'
 rm $TMPDIR/wat.sh
