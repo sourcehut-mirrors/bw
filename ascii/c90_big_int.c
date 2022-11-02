b75e1aa1ef4c42026131fb658cf28ebdf116f32e
Dennis Clarke <dclarke@blastwave.org>
2022-11-02 16:29:01 +0000

    20221102162901 : a big integer literal in C90 strict

---
+++ ascii/c90_big_int.c
@@ -0,0 +1,53 @@
+
+/*
+ * c90_big_int.c  test with a very large integer and strict C90.
+ * Copyright (C) Dennis Clarke 2022
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+ *********************************************************************/
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+int
+main(int argc, char **argv)
+{
+
+    uint64_t big_int0 = 0xFFFFFFFAFAFAFAFE;
+    uint64_t big_int1 = 0xFFFFFFFE00000001;
+
+    fprintf(stdout,"big_int0 = %" PRIu64 "\n", big_int0);
+    fprintf(stdout,"big_int1 = %" PRIu64 "\n", big_int1);
+
+    return EXIT_SUCCESS;
+
+}
+
