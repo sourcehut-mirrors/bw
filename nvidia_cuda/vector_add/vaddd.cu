950d488eeab960e2760ced6983d2db198d4a7f00
blastwave <dclarke@blastwave.org>
2022-12-11 03:12:52 +0000

    20221211031252 : for unknown reasons CUDA Event timers do not work for me

--- nvidia_cuda/vector_add/vaddd.cu
+++ nvidia_cuda/vector_add/vaddd.cu
@@ -43,6 +43,8 @@ int main(int argc, char *argv[])
     /* only you can stop the abuse of i as a loop counter */
     int j;
 
+    long candidate_int = 0;
+
     /* we shall assume that everything just works. */
     int exit_status = EXIT_SUCCESS;
 
@@ -51,6 +53,7 @@ int main(int argc, char *argv[])
      */
     double epsilon = 0.000000000000000888178419700125232338905334472656250;
     struct timespec t_start, t_end, t0, t1;
+    cudaEvent_t cuda_start, cuda_stop;
     uint64_t tdelta_nsec;
 
     /* we shall use this CUDA device error status over and over */
@@ -78,6 +81,21 @@ int main(int argc, char *argv[])
     t0.tv_sec = t_start.tv_sec;
     t0.tv_nsec = t_start.tv_nsec;
 
+    /* initialize the cuda event timers */
+    float cuda_milliseconds = 0.0;
+    cuda_err = cudaEventCreate(&cuda_start);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : failed to cudaEventCreate()\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        return EXIT_FAILURE;
+    }
+
+    cuda_err = cudaEventCreate(&cuda_stop);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : failed to cudaEventCreate()\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        return EXIT_FAILURE;
+    }
 
     /* determine the number of CUDA capable GPUs */
     cudaGetDeviceCount(&num_gpus);
@@ -90,6 +108,32 @@ int main(int argc, char *argv[])
     printf("INFO : number of host CPUs:\t%d\n", omp_get_num_procs());
     printf("INFO : number of CUDA devices:\t%d\n", num_gpus);
 
+
+
+    /* check if there is a numerical parameter on the CLI */
+    errno = 0;
+    if ( argc > 1 ) {
+        candidate_int = (int)strtol(argv[1], (char **)NULL, 10);
+        if ( ( errno == ERANGE ) || ( errno == EINVAL ) ){
+            fprintf(stderr,"FAIL : array size not understood\n");
+            perror("     ");
+            return EXIT_FAILURE;
+        }
+
+        /* 681740840 elements of 8-byte floating point in three arrays
+         * will likely fit into a 16G mem GPU. Barely */
+        if ( ( candidate_int < 16777216 ) || ( candidate_int > 681740840 ) ){
+            fprintf(stderr,"WARN : array size is unreasonable\n");
+            fprintf(stderr,"     : we shall assume 2^28 and proceed.\n");
+            numElements = 268435456;
+        } else {
+            numElements = candidate_int;
+            size = numElements * sizeof(double);
+        }
+
+        fprintf(stderr,"INFO : array size is %i\n", numElements);
+    }
+
     /* we need a device that can handle three arrays with some 
      * minimal overhead. Say 5% just for giggles. That can be
      * stupid large on a big NVidia Quadro */
@@ -99,7 +143,8 @@ int main(int argc, char *argv[])
 
                                          );
 
-    printf("     : we need %" PRIu64 " bytes of memory on a GPU\n", memory_fit_size);
+    printf("     : we need %" PRIu64 " bytes of memory on a GPU\n",
+                 memory_fit_size);
 
     cudaDeviceProp *dprop = (cudaDeviceProp *)calloc( num_gpus, sizeof(cudaDeviceProp));
     if ( dprop == NULL ) {
@@ -128,10 +173,6 @@ int main(int argc, char *argv[])
     int gpu_unit_max_number = -1;
     int gpu_unit_min_number = -1;
 
-    /* why is the NVidia Device properties name 256 bytes ? */
-    char gpu_max_unit_name[256] = "";
-    char gpu_min_unit_name[256] = "";
-
     for (j = 0; j < num_gpus; j++) {
 
         cudaGetDeviceProperties(dprop+j, j);
@@ -146,18 +187,12 @@ int main(int argc, char *argv[])
         if ( *(gpu_memory+j) > gpu_max_memory ) {
             gpu_max_memory = *(gpu_memory+j);
             gpu_unit_max_number = j;
-            gpu_max_unit_name[0] = '\0';
-            /* we do not need to copy the char name given that
-               the information is already in the struct cudaDeviceProp */
-            strncpy(gpu_max_unit_name,(dprop+j)->name,strlen((dprop+j)->name));
         }
 
         /* track the smallest memory device */
         if ( *(gpu_memory+j) < gpu_min_memory ) {
             gpu_min_memory = *(gpu_memory+j);
             gpu_unit_min_number = j;
-            gpu_min_unit_name[0] = '\0';
-            strncpy(gpu_min_unit_name,(dprop+j)->name,strlen((dprop+j)->name));
         }
 
     }
@@ -168,21 +203,27 @@ int main(int argc, char *argv[])
                           gpu_min_memory);
 
     printf("     : max memory unit is %i: %s with %" PRIu64 " bytes\n",
-                          gpu_unit_max_number, gpu_max_unit_name, gpu_max_memory);
+                          gpu_unit_max_number,
+                          (dprop+gpu_unit_max_number)->name,
+                          gpu_max_memory);
 
     /* For giggles we shall select the min unit and watch
      * it all blow up */
     cuda_err = cudaSetDevice(gpu_unit_min_number);
     if (cuda_err != cudaSuccess) {
-        fprintf(stderr, "FAIL : CUDA failed to select %s\n", gpu_max_unit_name);
+        fprintf(stderr, "FAIL : CUDA failed to select %s\n",
+                                        (dprop+gpu_unit_min_number)->name);
         fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
         return EXIT_FAILURE;
+    } else {
+        printf("     : selected %s\n", (dprop+gpu_unit_min_number)->name);
     }
 
     /*
     if (cudaSetDevice(gpu_unit_max_number) != cudaSuccess) {
         cuda_err = cudaGetLastError();
-        fprintf(stderr, "FAIL : CUDA failed to select %s\n", gpu_max_unit_name);
+        fprintf(stderr, "FAIL : CUDA failed to select %s\n",
+                                        (dprop+gpu_unit_max_number)->name);
         fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
         exit(EXIT_FAILURE);
     }
@@ -237,17 +278,18 @@ int main(int argc, char *argv[])
 
     /* Allocate the device input vectors */
     double *d_A = NULL;
-    if (cudaMalloc((void **)&d_A, size) != cudaSuccess) {
+    cuda_err = cudaMalloc((void **)&d_A, size);
+    if (cuda_err != cudaSuccess) {
         cuda_err = cudaGetLastError();
         fprintf(stderr, "FAIL : CUDA failed to allocate vector A\n");
         fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
         return EXIT_FAILURE;
     }
+
     clock_gettime( CLOCK_REALTIME, &t0 );
     tdelta_nsec = timediff( t1, t0);
     printf("     : Wallclock cudaMalloc(A) %10" PRIu64 " nsecs  %9.7g secs\n",
                                tdelta_nsec, (float)tdelta_nsec/1.0e9);
-
 
     double *d_B = NULL;
     if (cudaMalloc((void **)&d_B, size) != cudaSuccess) {
@@ -267,16 +309,6 @@ int main(int argc, char *argv[])
         cuda_err = cudaGetLastError();
         fprintf(stderr, "FAIL : CUDA failed to allocate vector C\n");
         fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
-
-        /* TODO : figure out what the Out of Memory error value
-         *        really is
-         *
-        printf("cuda_err is at address %p\n\n", &cuda_err);
-        for ( j=0; j<sizeof(cudaError_t); j++ ) {
-            printf("%02x ", ((unsigned char *)&cuda_err)[j] );
-        }
-        printf("\n" );
-        */
 
         if ( cuda_err == cudaErrorMemoryAllocation ) {
             fprintf(stderr, "     : cuda_err = cudaErrorMemoryAllocation\n");
@@ -326,6 +358,27 @@ int main(int argc, char *argv[])
     printf("INFO : CUDA kernel launch with %d blocks of %d threads\n",
                     blocksPerGrid, threadsPerBlock);
 
+
+    /* this sucks -- always throws an error cudaErrorInvalidResourceHandle
+
+
+       ***********************************************************
+       *   THE CUDA Event timer stuff does not work at all. At   *
+       *   least not for me. I have no idea why.                 *
+       ***********************************************************
+
+    cuda_err = cudaEventRecord(cuda_start);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : failed to cudaEventRecord(cuda_start)\n");
+        fprintf(stderr, "     : error %s\n", cudaGetErrorString(cuda_err));
+        fprintf(stderr, "     : at %d in %s\n", __LINE__, __FILE__);
+        free(h_A);
+        free(h_B);
+        free(h_C);
+        return EXIT_FAILURE;
+    }
+    */
+
     /*******************************************************
      * Special NOTE : this is asynchronous where control   *
      *                returns immediately to the CPU.      *
@@ -349,6 +402,35 @@ int main(int argc, char *argv[])
                             tdelta_nsec, (float)tdelta_nsec/1.0e9);
 
 
+
+    /* this also suck ass 
+    cuda_err = cudaEventRecord(cuda_stop);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : failed to cudaEventRecord(cuda_stop)\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        fprintf(stderr, "     : at %d in %s\n", __LINE__, __FILE__);
+        free(h_A);
+        free(h_B);
+        free(h_C);
+        return EXIT_FAILURE;
+    }
+    */
+
+    /* none of this works
+
+    cuda_err = cudaEventElapsedTime(&cuda_milliseconds, cuda_start, cuda_stop);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : failed to cudaEventElapsedTime\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        fprintf(stderr, "     : at %d in %s\n", __LINE__, __FILE__);
+        free(h_A);
+        free(h_B);
+        free(h_C);
+        return EXIT_FAILURE;
+    }
+    printf("     : cudaEventElapsedTime claims %9.7g secs\n",
+                               cuda_milliseconds * 1000.0);
+    */
 
     /* Copy the device result vector in device memory to the host
      * result vector in host memory */
@@ -402,11 +484,30 @@ int main(int argc, char *argv[])
         exit_status = EXIT_FAILURE;
     }
 
+    cuda_err = cudaEventDestroy(cuda_start);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : Failed to cudaEventDestroy(cuda_start)\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        /* we do not need to bail out here 
+        exit_status = EXIT_FAILURE;
+        */
+    }
+
+    cuda_err = cudaEventDestroy(cuda_stop);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "FAIL : Failed to cudaEventDestroy(cuda_stop)\n");
+        fprintf(stderr, "FAIL : error %s\n", cudaGetErrorString(cuda_err));
+        /* we do not need to bail out here 
+        exit_status = EXIT_FAILURE;
+        */
+    }
+
 
     /* Someday we may actually use exit_status but today is not that day */
     if ( exit_status != EXIT_SUCCESS ) {
         fprintf(stderr, "FAIL : Something went wrong somewhere. Good Luck.\n");
     }
+
 
     /* Free host memory */
     free(h_A);
