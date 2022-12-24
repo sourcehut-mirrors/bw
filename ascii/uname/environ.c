233cb7c4aa737f4ad7936b3cde81e36f78621cd4
blastwave <dclarke@blastwave.org>
2022-12-24 02:36:50 +0000

    20221224023650 : walk the environ string array

---
+++ ascii/uname/environ.c
@@ -0,0 +1,84 @@
+/*
+ * environ.c    Walk the char **environ
+ *
+ * Copyright (C) Dennis Clarke 2022
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+ *********************************************************************/
+#define _XOPEN_SOURCE 600
+
+#include <errno.h>
+#include <locale.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+extern char **environ;
+
+int main(int argc, char *argv[])
+{
+
+    int j = 0;
+    size_t k;
+    struct utsname uname_data;
+    char *env_var_value = NULL;
+
+    char *env_var[] = {"UNAME_s","UNAME_r","UNAME_v","UNAME_m"};
+
+    setlocale( LC_MESSAGES, "C" );
+
+    if ( environ != NULL ) {
+        printf ("INFO : environ is at %p\n", &environ);
+    } else {
+        printf ("FAIL : environ is NULL\n");
+        return EXIT_FAILURE;
+    }
+
+    env_var_value = *environ;
+
+    while ( env_var_value != NULL ) {
+        j = j + 1;
+        printf ("[%-3i]: length %-3i = \"%s\"\n     : ",
+                j, strlen(env_var_value), env_var_value);
+
+        for ( k = 0; k < strlen(env_var_value); k++ ) {
+            printf("%02x ", ((uint8_t*)env_var_value)[k] );
+            if ( ( k > 15 ) && ( k%16 == 0 ) ) printf ("\n     : ");
+        }
+        printf("\n\n");
+
+        env_var_value = *(environ + j);
+
+    }
+
+    return EXIT_SUCCESS;
+
+}
+
