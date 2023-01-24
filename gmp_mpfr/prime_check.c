d6ed96a28fc08ab6ac9bde3355a148c5b8bc987c
blastwave <dclarke@blastwave.org>
2023-01-24 20:30:06 +0000

    20230124203006 : attempt to use gmp_snprintf poorly

--- gmp_mpfr/prime_check.c
+++ gmp_mpfr/prime_check.c
@@ -1,4 +1,47 @@
 
+/* prime_check.c    The GMP manual seems to list a few functions in a
+ *                  category named "Number Theoretic Functions". These
+ *                  include mpz_probab_prime_p() and mpz_nextprime().
+ *                  The names seem fairly clear in that we may ask for
+ *                  the probability that a number is prime as well as
+ *                  ask for the next prime larger than some number n.
+ *                  One of them seems to work whereas the other may only
+ *                  work somewhat. See : 
+ *
+ *          https://gmplib.org/manual/Number-Theoretic-Functions
+ *
+ *                  The test number that concerns me is : 
+ *
+ *     n = 93461639715357977769163558199606896584051237541638188580280321
+ *
+ * Copyright (C) Dennis Clarke 2023
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+/*******************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *  An XSI-conforming application should ensure that the feature
+ *  test macro _XOPEN_SOURCE is defined with the value 600 before
+ *  inclusion of any header. This is needed to enable the
+ *  functionality described in The _POSIX_C_SOURCE Feature Test
+ *  Macro and in addition to enable the XSI extension.
+ *******************************************************************/
 #define _XOPEN_SOURCE 600
 
 #include <errno.h>
@@ -24,40 +67,84 @@ int main (int argc, char *argv[]) {
 
     mpz_t prime_check_input, less_one, next_prime;
 
-    char *prime_check_string;
-    int prime_check_reps, prime_check_test;
+    char *cli, *cli_verify;
+    size_t len;
+    int prime_check_reps, prime_check_test, cli_check;
 
     setlocale(LC_ALL, "C");
 
     /* argv[1] must provide an integer */
-
     if ( argc < 1 ) {
         fprintf(stderr,"please provide a decimal integer\n");
         return EXIT_FAILURE;
     }
 
-
     /* for the sake of quick testing we shall assume
-     * that the user did provide a decimal number
-     */
-    prime_check_string = calloc(strlen(argv[1])+1,sizeof(unsigned char));
+     * that the user did provide a decimal number */
+    errno = 0;
+    len = strlen(argv[1]);
+    cli = calloc(len+1,sizeof(unsigned char));
+    if ( cli == NULL ) {
+        fprintf(stderr,"BORK BORK BORK at %d in %s\n", __LINE__, __FILE__);
+        perror("ERR ");
+        return EXIT_FAILURE;
+    }
+
+    errno = 0;
+    cli_verify = calloc(len+1,sizeof(unsigned char));
+    if ( cli_verify == NULL ) {
+        fprintf(stderr,"BORK BORK BORK at %d in %s\n", __LINE__, __FILE__);
+        perror("ERR ");
+        return EXIT_FAILURE;
+    }
 
     /* initialize the libgmp data struct */
     mpz_init(prime_check_input);
 
     /* we will likely want to check if the input was
      * accepted and processed correctly. Therefore we
-     * need a copy of the input string. */
-    strncpy(prime_check_string,argv[1],strlen(argv[1]));
+     * need a copy of the input string.
+     * NOTE : would it not be lovely to use a variable
+     *        name such as : 
+     *
+     *  char *command_line_interface_user_input_argument_1
+     *
+     * However my old old Fortran77 punch card brain seems
+     * to just cut loose a high pitch scream at that.
+     */
+    strncpy(cli,argv[1],len);
 
-    if ( mpz_set_str(prime_check_input, prime_check_string, 10) < 0 ) {
+    if ( mpz_set_str(prime_check_input, cli, 10) < 0 ) {
         /* well something went horribly wrong with the input */
-        printf("\nBORK BORK BORK at %d in %s\n", __LINE__, __FILE__);
+        fprintf(stderr,"BORK BORK BORK at %d in %s\n", __LINE__, __FILE__);
         return EXIT_FAILURE;
     }
 
-    printf("INFO : prime_check_string is \"%s\"\n",prime_check_string);
+    /* this is a visual eyeball check if the input matches the 
+     * data represented inside the mpz_t struct
+    printf("INFO : cli is \"%s\"\n",cli);
     gmp_printf ("     : gmp_printf mpz reports %Zd\n", prime_check_input);
+
+     we have the beautiful func : 
+
+        int gmp_snprintf (char *buf, size_t size, const char *fmt, ...)
+
+            so lets use it 
+
+     */
+
+    cli_check = gmp_snprintf(cli_verify, len, "%Zd");
+
+    if ( cli_check != len ) {
+        fprintf(stderr,"BORK BORK BORK\n");
+        fprintf(stderr,"\n\nThe input string was \n\n    \"");
+        fprintf(stderr,"%s\"\n\n", cli);
+        fprintf(stderr,"\n\ngmp_snprintf() resulted in \n\n    \"");
+        fprintf(stderr,"%s\"\n\n", cli_verify);
+        fprintf(stderr,"BORK BORK BORK\n");
+        return EXIT_FAILURE;
+    }
+
 
     /*
      * Function: int mpz_probab_prime_p (const mpz_t n, int reps)
