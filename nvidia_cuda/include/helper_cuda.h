adbf3aa06b0dcb0f3cf02ad5c9e71d269f557c3f
Dennis Clarke <dclarke@blastwave.org>
2023-06-25 15:58:56 +0000

    20230625155856 : attempt to compute the number of CUDA cores

--- nvidia_cuda/include/helper_cuda.h
+++ nvidia_cuda/include/helper_cuda.h
@@ -1089,7 +1089,8 @@ inline int _ConvertSMVer2Cores(int major, int minor)
     }
 
     /* If we don't find the values, we default use the previous one to run properly */
-    printf("MapSMtoCores for SM %d.%d is undefined.  Default to use %d Cores/SM\n", major, minor, nGpuArchCoresPerSM[index-1].Cores);
+    fprintf(stderr,"WARN : MapSMtoCores for SM %d.%d is undefined.\n", major, minor);
+    fprintf(stderr,"     : Default to use %d Cores/SM\n", nGpuArchCoresPerSM[index-1].Cores);
     return nGpuArchCoresPerSM[index-1].Cores;
 }
 // end of GPU Architecture definitions
@@ -1142,7 +1143,7 @@ inline int gpuDeviceInit(int devID)
     return devID;
 }
 
-// This function returns the best GPU (with maximum GFLOPS)
+/* This function returns the best GPU (with maximum GFLOPS) */
 inline int gpuGetMaxGflopsDeviceId()
 {
     int current_device     = 0, sm_per_multiproc  = 0;
@@ -1156,42 +1157,34 @@ inline int gpuGetMaxGflopsDeviceId()
     
     checkCudaErrors(cudaGetDeviceCount(&device_count));
 
-    if (device_count == 0)
-    {
-        fprintf(stderr, "gpuGetMaxGflopsDeviceId() CUDA error: no devices supporting CUDA.\n");
+    if (device_count == 0) {
+        fprintf(stderr, "FAIL : gpuGetMaxGflopsDeviceId() CUDA error: no devices supporting CUDA.\n");
         exit(EXIT_FAILURE);
     }
 
-    // Find the best major SM Architecture GPU device
-    while (current_device < device_count)
-    {
+    /* Find the best major SM Architecture GPU device */
+    while (current_device < device_count) {
         cudaGetDeviceProperties(&deviceProp, current_device);
 
-        // If this GPU is not running on Compute Mode prohibited, then we can add it to the list
-        if (deviceProp.computeMode != cudaComputeModeProhibited)
-        {
-            if (deviceProp.major > 0 && deviceProp.major < 9999)
-            {
+        /* only if this GPU is not running on Compute Mode prohibited */
+        if (deviceProp.computeMode != cudaComputeModeProhibited) {
+            if (deviceProp.major > 0 && deviceProp.major < 9999) {
                 best_SM_arch = MAX(best_SM_arch, deviceProp.major);
             }
-        }
-        else
-        {
+        } else {
             devices_prohibited++;
         }
 
         current_device++;
     }
 
-    if (devices_prohibited == device_count)
-    {
-    	fprintf(stderr, "gpuGetMaxGflopsDeviceId() CUDA error: all devices have compute mode prohibited.\n");
+    if (devices_prohibited == device_count) {
+    	fprintf(stderr, "FAIL : gpuGetMaxGflopsDeviceId() CUDA error: all devices have compute mode prohibited.\n");
     	exit(EXIT_FAILURE);
     }
 
-    // Find the best CUDA capable GPU device
+    /* Find the best CUDA capable GPU device */
     current_device = 0;
-
     while (current_device < device_count)
     {
         cudaGetDeviceProperties(&deviceProp, current_device);
--- nvidia_cuda/mandel_hack/mandel_hack.cu
+++ nvidia_cuda/mandel_hack/mandel_hack.cu
@@ -35,7 +35,21 @@
 
 #define NUM_ELEMENTS 1048576
 #define THREADS_PER_BLOCK 1024
+#define IMG_PIX_W 1024
 
+/* dataset 0 
+#define BAIL_OUT 256
+#define MAGNIFY 128
+#define REAL_COORD -1.225891113281250
+#define IMAG_COORD -0.177673339843750
+*/
+
+/* dataset 1 
+#define BAIL_OUT 32768
+#define MAGNIFY 268435456
+#define REAL_COORD 0.399750960350502282381
+#define IMAG_COORD 0.205251797480741515756
+*/
 
 /* dataset 2 *
 #define BAIL_OUT 32768
@@ -43,21 +57,14 @@
 #define REAL_COORD -0.0157887752805699992098
 #define IMAG_COORD  1.02061921088799989477
 */
-#define IMG_PIX_W 1024
 
-/* this is the reference dataset 1 
-#define BAIL_OUT 32768
-#define MAGNIFY 268435456
-#define REAL_COORD 0.399750960350502282381
-#define IMAG_COORD 0.205251797480741515756
-*/
-
-/* dataset 0 
-    256 128 -1.225891113281250 -0.177673339843750 */
-#define BAIL_OUT 256
-#define MAGNIFY 128
-#define REAL_COORD -1.225891113281250
-#define IMAG_COORD -0.177673339843750
+/* dataset 5
+   524288 4398046511104 -0.0157887752802652429895 1.02061921090827745218
+   */
+#define BAIL_OUT 524288
+#define MAGNIFY 4398046511104
+#define REAL_COORD -0.0157887752802652429895
+#define IMAG_COORD 1.02061921090827745218
 
 /* TODO read in the established data files */
 #define VBOX_REAL_COUNT 16
@@ -259,12 +266,22 @@ int main(int argc, char *argv[])
         }
 
         cudaGetDeviceProperties(&dprop, i);
-        printf("     :    %d: %s    %12" PRIu64 " totalGlobalMem  %6i clockRate   %4i memoryBusWidth   %4i multiProcessorCount    %4i maxThreadsPerMultiProcessor   %i cores\n",
-                i, dprop.name, dprop.totalGlobalMem, dprop.clockRate,
-                dprop.memoryBusWidth, dprop.multiProcessorCount,
-                dprop.maxThreadsPerMultiProcessor,
-                ConvertSMVer2Cores(dprop.major, dprop.minor)
-                );
+
+        printf("     :    %d: %s\n", i, dprop.name);
+
+        printf("     :        %12" PRIu64 " totalGlobalMem\n", dprop.totalGlobalMem);
+
+        printf("     :             %6i clockRate\n", dprop.clockRate);
+
+        printf("     :                %4i memoryBusWidth\n", dprop.memoryBusWidth);
+
+        printf("     :                %4i multiProcessorCount\n", dprop.multiProcessorCount);
+
+        printf("     :                %4i maxThreadsPerMultiProcessor\n", dprop.maxThreadsPerMultiProcessor);
+
+        printf("     :               %5i cores\n",
+                _ConvertSMVer2Cores(dprop.major, dprop.minor) * dprop.multiProcessorCount);
+
     }
 
     /* the above Scheiße will leave us with the highest number
