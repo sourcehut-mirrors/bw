5e060c57a686b4ae6901ae656ac80b2115376667
Dennis Clarke <dclarke@blastwave.org>
2023-07-01 16:18:08 +0000

    20230701161808 : make a trivial template for timing data

--- xmand/timings_ds2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-grep 'nsec' output/readme.dataset2* | cut -f2 -d\:  | grep -v 'INFO' | cut -f2 -d\=  | sort -n | awk 'BEGIN{print"#!/bin/sh"}{print "/usr/bin/printf \047" $1 "    \047\ngrep \047" $1 "\047 output/readme.dataset2* | cut -f1 -d\: "}' > $TMPDIR/wat.sh; chmod 0755 $TMPDIR/wat.sh
-$TMPDIR/wat.sh | sed -e 's/output\/readme\.dataset2_//' | awk '{ printf("%14s    %s\n", $1, $2)}'
-rm $TMPDIR/wat.sh
