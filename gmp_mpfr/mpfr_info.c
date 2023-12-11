e62d550b8ed81f4003b06bb8308c2756971c7dd3
blastwave <dclarke@blastwave.org>
2023-12-11 08:54:03 +0000

    20231211085403 : really trivial mpfr and gmp version stuff

---
+++ gmp_mpfr/mpfr_info.c
@@ -0,0 +1,93 @@
+
+/* mpfr_info.c   Some mpfr version information
+ *
+ * ------------------------------------------------------------------
+ * Copyright (c) 2017 Dennis Clarke
+ *
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or
+ *    sell copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ *        KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *        PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *        OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ *        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+/*******************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *  An XSI-conforming application should ensure that the feature
+ *  test macro _XOPEN_SOURCE is defined with the value 600 before
+ *  inclusion of any header. This is needed to enable the
+ *  functionality described in The _POSIX_C_SOURCE Feature Test
+ *  Macro and in addition to enable the XSI extension.
+ *******************************************************************/
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <gmp.h>
+#include <mpfr.h>
+
+#define VERBOSE 1
+
+int
+main(int argc, char *argv[])
+{
+
+    mpfr_prec_t prec;
+
+    printf("GMP  library version : %d.%d.%d\n",
+            __GNU_MP_VERSION,
+            __GNU_MP_VERSION_MINOR,
+            __GNU_MP_VERSION_PATCHLEVEL );
+
+    printf("MPFR library: %-12s\n", mpfr_get_version ());
+    printf("MPFR header : %s (based on %d.%d.%d)\n",
+            MPFR_VERSION_STRING,
+            MPFR_VERSION_MAJOR,
+            MPFR_VERSION_MINOR,
+            MPFR_VERSION_PATCHLEVEL);
+
+    if (mpfr_buildopt_tls_p()!=0)
+        printf("            : compiled as thread safe using TLS\n");
+
+    if (mpfr_buildopt_float128_p()!=0) 
+        printf("            : __float128 support enabled\n");
+
+    if (mpfr_buildopt_decimal_p()!=0)
+        printf("            : decimal float support enabled\n");
+
+    if (mpfr_buildopt_gmpinternals_p()!=0)
+        printf("            : compiled with GMP internals\n");
+
+    if (mpfr_buildopt_sharedcache_p()!=0)
+        printf("            : threads share cache per MPFR const\n");
+
+    printf("            : sizeof(mpfr_prec_t) = %i\n",
+                                      sizeof(mpfr_prec_t));
+
+    printf("MPFR thresholds file used at compile time : %s\n\n",
+                                      mpfr_buildopt_tune_case ());
+
+    return EXIT_SUCCESS;
+
+}
+
