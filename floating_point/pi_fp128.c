ba17b632eb7bcfbcaf931934215e25aec2887989
blastwave <dclarke@blastwave.org>
2024-03-19 21:36:05 +0000

    20240319213605 : just another test on ye old NetBSD sparcv9

--- floating_point/pi_fp128.c
+++ floating_point/pi_fp128.c
@@ -3,22 +3,30 @@
  * pi_fp128.c  Demonstrate that some architectures can not process
  *             the standard IEEE754-2008 data types.
  *
- * Copyright (C) Dennis Clarke 2019
+ * ------------------------------------------------------------------
+ * Copyright (c) 2019 Dennis Clarke
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or
+ *    sell copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- * https://www.gnu.org/licenses/gpl-3.0.txt
+ *        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ *        KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *        PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *        OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ *        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ------------------------------------------------------------------
  */
 
 /*********************************************************************
@@ -32,29 +40,33 @@
  *    Macro and in addition to enable the XSI extension.
  *
  *********************************************************************/
-
+#if ! defined (_XOPEN_SOURCE)
 #define _XOPEN_SOURCE 600
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 
-    long double foobar = 3.14159265358979323846264338327950288419716939937510L;
+    int j = 0;
+    long double pi = 3.14159265358979323846264338327950288419716939937510L;
 
-    printf("the sizeof(foobar) is %i bytes\n", sizeof(long double) );
+    printf("the sizeof(pi) is %i bytes\n", sizeof(long double) );
 
-    printf("\nfoobar is this  3.14159265358979323846264338");
+    printf("\npi is this  3.14159265358979323846264338");
     printf("327950288419716939937510.....\n\n");
 
-    printf("foobar could be %42.38Le\n\n", foobar);
+    printf("pi could be %42.38Le\n\n", pi);
 
-    /* note hex representation of foobar is 
-     *   0x4000 0x921f 0xb544 0x42d1 0x8469 0x898c 0xc517 0x01b8 */
+    printf("Actual hex data for 128bit pi is\n\n");
+    printf("  0x40 00 92 1f b5 44 42 d1 84 69 89 8c c5 17 01 b8\n\n");
 
-    printf("foobar is at address %p\n\n", &foobar);
-    for ( int j=0; j<sizeof(long double); j++ )
-        printf("%02x ", ((unsigned char *)&foobar)[j] );
+    printf("Our pi data is at address %p\n\n  0x", &pi);
+    for ( j=0; j<sizeof(long double); j++ ) {
+        printf("%02x ", ((unsigned char *)&pi)[j] );
+    }
 
     printf("\n" );
 
