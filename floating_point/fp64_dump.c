2e34d0ba8a2db09c23016c717344f2205cd2fe53
blastwave <dclarke@blastwave.org>
2024-05-13 03:53:52 +0000

    20240513035349 : detect endian state without type punning and C90 clean

--- floating_point/fp64_dump.c
+++ floating_point/fp64_dump.c
@@ -27,23 +27,21 @@
  */
 
 /*********************************************************************
- * The Open Group Base Specifications Issue 6
- * IEEE Std 1003.1, 2004 Edition
- *
- *    An XSI-conforming application should ensure that the feature
- *    test macro _XOPEN_SOURCE is defined with the value 600 before
- *    inclusion of any header. This is needed to enable the
- *    functionality described in The _POSIX_C_SOURCE Feature Test
- *    Macro and in addition to enable the XSI extension.
+ *    The intention here is that the code be strictly compliant
+ *    with IEEE  Std  1003.1-200x and earlier for ISO/IEC 9899:1990
+ *    spec C.
  *
  *********************************************************************/
 #if ! defined (_XOPEN_SOURCE)
-#define _XOPEN_SOURCE 600
+#define _XOPEN_SOURCE 500
 #endif
 
 #include <errno.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <unistd.h>
 
 /*
@@ -53,6 +51,31 @@
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
 */
+
+int
+what_is (char *some_string)
+{
+    /* on systems that strictly conform to the C90 spec
+     * we will get an integer return type. If we use C99
+     * then we get a floating point value. */
+    char *end_ptr;
+    double fp64;
+    long long_int;
+
+    fp64 = strtod(some_string, &end_ptr);
+
+    if ( (some_string != end_ptr) && (*end_ptr == '\0')) {
+        printf("It's a double with value %g\n", fp64);
+    } else {
+        long_int = strtol(some_string, &end_ptr, 0);
+        if (some_string != end_ptr && *end_ptr == '\0') {
+            printf("It's an integer with value %ld\n", long_int);
+        } else {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 /* Accept a pointer to a pointer of some data element which
  * is expected to be an IEEE 754-2008 floating point FP64
@@ -77,45 +100,51 @@
  *
  */
 
-int what_is (char *some_string)
+int fp64_dump ( double *some_fp64 )
 {
-    char *end_ptr;
-    double fp64;
-    long long_int;
 
-    fp64 = strtod(some_string, &end_ptr);
+    unsigned char eight_byte[8];
+    size_t len_double = sizeof(double);
+    size_t len_int = sizeof(int);
+    int j = 1;
+    int little_endian;
+    void *some_ptr;
+    void *int_ptr = (void *)&j;
+    void *src_ptr = (void *)some_fp64;
+    void *dst_ptr = (void *)&eight_byte;
 
-    if ( (some_string != end_ptr) && (*end_ptr == '\0')) {
-        printf("It's a float with value %g\n", fp64);
-    } else {
-        long_int = strtol(some_string, &end_ptr, 0);
-        if (some_string != end_ptr && *end_ptr == '\0') {
-            printf("It's an integer with value %ld\n", 1);
-        } else {
-            return 1;
-        }
+
+    /* this is bad ju ju
+     * Type punning is a bad idea.
+     *    int little_endian = (*(uint8_t*)&j == 1) ? 1 : 0;
+     */
+
+    some_ptr = memcpy(dst_ptr, int_ptr, len_int);
+    for ( j=0; j<len_int; j++ ) {
+        printf("eight_byte[%i] = %i\n", j, eight_byte[j]);
     }
-    return 0;
+    little_endian = eight_byte[0];
+    printf("\nlittle_endian = eight_byte[0] = %i\n",little_endian);
+
+
+    printf("\naddr %p  :", src_ptr);
+    some_ptr = memcpy(dst_ptr, src_ptr, len_double);
+    for ( j=0; j<len_double; j++ ) {
+        printf(" %02x", eight_byte[j]);
+    }
+    printf("\n");
+
+
+    return EXIT_SUCCESS;
+
 }
 
-int fp64_dump ( double ** some_fp64 )
+int
+main ( int argc, char **argv )
 {
 
-    return 0;
-
-}
-
-int main ( int argc, char **argv )
-{
-
-    int k;
-
-    if ( argc < 2 ) {
-        fprintf(stderr,"Gimme a string dummy.\n");
-        return EXIT_FAILURE;
-    }
-
-    k = what_is(argv[1]);
+    double pi = M_PI;
+    int foo = fp64_dump( &pi );
 
     return EXIT_SUCCESS;
 
