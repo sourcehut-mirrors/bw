ba837af1c3230bee14dc4745907100678bcfb75f
blastwave <dclarke@blastwave.org>
2025-03-01 03:24:53 +0000

    20250301032453 : still not sure about SPARC64 and the popc

--- in_progress/popcount/bob_sparc64.s
+++ in_progress/popcount/bob_sparc64.s
@@ -5,15 +5,7 @@
 	.type	answer, #function
 	.proc	04
 answer:
-	save	%sp, -176, %sp
-	mov	%i0, %g1
-	st	%g1, [%fp+2175]
-	ld	[%fp+2175], %g1
-	srl	%g1, 0, %g1
-	popc	%g1, %g1
-	sra	%g1, 0, %g1
-	mov	%g1, %i0
-	return	%i7+8
-	 nop
+	jmp	%o7+8
+	 popc	%o0, %o0
 	.size	answer, .-answer
 	.ident	"GCC: (genunix Fri May 11 08:23:40 GMT 2018) 8.1.0"
