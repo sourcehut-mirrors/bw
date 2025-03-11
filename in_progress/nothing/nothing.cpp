1cf1d84b9cffc558e62162f8ffd46699aa671ef5
Dennis M. Clarke <dclarke@triton.bw.genunix.com>
2025-03-11 00:53:44 +0000

    20250311005344 : a nothing iostream C++ test

---
+++ in_progress/nothing/nothing.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+/* This test rejects OSF 5.1 Compaq C++ in its default pre-standard iostream
+   mode, since that mode puts cout in the global namespace, not "std".  */
+void someoutput (void) { std::cout << 123; }
+
+int main (void) { return 0; }
