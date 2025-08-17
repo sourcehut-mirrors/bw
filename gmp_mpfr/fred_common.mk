5f7a969dd481a320eb244e3f465dbe91c8249380
blastwave <dclarke@blastwave.org>
2025-08-17 01:58:40 +0000

    20250817015840 : same Fred Gruenberger test on FreeBSD 14.3 Xeon at a million digits

---
+++ gmp_mpfr/fred_common.mk
@@ -0,0 +1,24 @@
+
+CPPFLAGS?= -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=600
+
+LDIR?=	/opt/bw/lib
+IDIR?=	/opt/bw/include
+ITIME?=	../time_and_date
+
+LIBS?=	-lgmp -lmpfr
+
+SRCS = ../sysinfo/sysinfo.c ../sysinfo/endian.c \
+	../time_and_date/tdiff.c gmp_mpfr_ver.c fred.c
+
+OBJS = ../sysinfo/sysinfo.o ../sysinfo/endian.o \
+	../time_and_date/tdiff.o gmp_mpfr_ver.o fred.o
+
+.c.o:
+	$(CC) -c -o $@ $< $(CFLAGS) -I$(IDIR) -I$(ITIME) $(CPPFLAGS)
+
+fred: $(OBJS)
+	$(CC) -o $@ $(OBJS) $(CFLAGS) -Wl,-rpath=$(LDIR) \
+	-Wl,-enable-new-dtags $(CPPFLAGS) -L$(LDIR) $(LIBS)
+
+clean:
+	rm -f $(OBJS) fred
