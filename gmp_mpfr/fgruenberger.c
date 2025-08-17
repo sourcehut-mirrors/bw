cd2bdb44f56e7a05076cc220276a9f58464671bd
blastwave <dclarke@blastwave.org>
2025-08-17 23:00:05 +0000

    20250817230005 : the final mpz_mul time is wrong

--- gmp_mpfr/fgruenberger.c
+++ gmp_mpfr/fgruenberger.c
@@ -68,15 +68,45 @@ main ( int argc, char **argv )
 {
 
     int j, loop_limit, err_clock;
-    size_t num_bytes, buf_len;
+    size_t num_bytes;
     long candidate_input;
-    struct timespec tn_begin, tn_0, tn_1;
+    struct timespec tn_begin, tn_0, tn_1, tn_end;
     tdiff_type delta_time;
-    double total_time = 0.0;
+
+    /* for the sake of plotting a graph we shall track the compute
+     * times per iteration */
+    double fg[29], mgst[29];
+    double mpz_mul_time = 0.0;
+    double mpz_get_str_time = 0.0;
+
     mpz_t g0, g1;
-    char *out_buf = NULL;
+    char *gmp_out_buf = NULL;
+
+    /* The libGMP call to create the char string output requires
+     * a NULL *char ptr as a parameter. Otherwise weird things
+     * happen. At least for me I saw a sigSEGV nasty stuff. */
     char *null_ptr = NULL;
+
+    /* This next little buffer wwill be for the actual char string
+     * to be dumped onto stdout. Not the whole damn result as that
+     * would be out of control massive. Thing is a GB of digits. */
     char *prn_buf = NULL;
+
+    /* We may or may not have CLOCK_MONOTONIC implemented and thus
+     * we test for what does exist and set a clockid_t type. */
+    clockid_t clock_flag;
+
+    /* The LLVM/Clang compiler can be a real whiner about
+     * things declared and not defined. Thus this is a way
+     * to tell the compiler to shut up. */
+    tn_0.tv_sec = 0;
+    tn_0.tv_nsec = 0;
+    tn_1.tv_sec = 0;
+    tn_1.tv_nsec = 0;
+    tn_begin.tv_sec = 0;
+    tn_begin.tv_nsec = 0;
+    tn_end.tv_sec = 0;
+    tn_end.tv_nsec = 0;
 
     if ( sysinfo(VERBOSE) == SYSINFO_FAIL ) {
         fprintf(stderr,"WARN : we may not have valid system info.\n");
@@ -87,10 +117,6 @@ main ( int argc, char **argv )
             __GNU_MP_VERSION_MINOR,
             __GNU_MP_VERSION_PATCHLEVEL );
     printf("-------------------------------------------------------------------\n");
-
-
-    /* we may or may not have CLOCK_MONOTONIC implemented */
-    clockid_t clock_flag;
 
     errno = 0;
     clock_flag = CLOCK_MONOTONIC;
@@ -130,8 +156,8 @@ main ( int argc, char **argv )
         if ( ( errno == ERANGE ) || ( errno == EINVAL ) ) {
             fprintf(stderr,"WARN : loop limit not understood\n");
             perror("     ");
-            fprintf(stderr,"     : we shall assume 12 loops.\n");
-            loop_limit = 12;
+            fprintf(stderr,"     : we shall assume 8 loops.\n");
+            loop_limit = 8;
         }
 
         if ( ( candidate_input < 2 ) || ( candidate_input > 28 ) ) {
@@ -144,13 +170,15 @@ main ( int argc, char **argv )
 
     } else {
         fprintf(stderr,"WARN : no loop limit entered\n");
-        fprintf(stderr,"     : we shall assume 12 loops\n");
-        loop_limit = 12;
+        fprintf(stderr,"     : we shall assume 8 loops\n");
+        loop_limit = 8;
     }
     fprintf(stderr,"\n");
 
-    mpz_inits ( g0, g1, NULL);
-    mpz_set_ui ( g0, 10000001);
+    mpz_inits(g0, g1, NULL);
+    mpz_set_ui(g0, 10000001);
+    /* we know that initially the compute time is zero */
+    fg[0] = 0.0;
 
     prn_buf = calloc(80,sizeof(unsigned char));
     if ( prn_buf == NULL ) {
@@ -158,63 +186,95 @@ main ( int argc, char **argv )
         return EXIT_FAILURE;
     }
 
-    out_buf = mpz_get_str(null_ptr, 10, g0);
-    if ( out_buf == NULL ) {
+    err_clock = clock_gettime(clock_flag, &tn_0);
+    gmp_out_buf = mpz_get_str(null_ptr, 10, g1);
+    err_clock = clock_gettime(clock_flag, &tn_1);
+    err_clock = tdiff( &delta_time, tn_0, tn_1);
+    mpz_get_str_time += delta_time.delta;
+    mgst[0] = delta_time.delta;
+
+    if ( gmp_out_buf == NULL ) {
         fprintf(stderr,"FAIL : mpz_get_str()\n");
         return EXIT_FAILURE;
     } else {
-        num_bytes = strlen(out_buf);
-        /* this is where OpenSSL can be used to get a SHA512
-         * hash of the data */
-        printf ("%3i    %s\n", 1, out_buf);
-
+        num_bytes = strlen(gmp_out_buf);
         /* printf("INFO : mpz_get_str() returns %li bytes\n", (int)num_bytes); */
 
-        free(out_buf);
-        out_buf = NULL;
+        /* This is where OpenSSL can be used to get a SHA512
+         * hash of the data in gmp_out_buf however we display
+         * only 72 chars of that. For obvious reasons. */
+        strncpy(prn_buf, gmp_out_buf, 72);
+        printf("  1    : %s\n", prn_buf);
+        printf("    %14i digits\n\n", (int)num_bytes);
+        /* slide in a dirty nul char at byte 0 */
+        prn_buf[0]='\0';
+
+        free(gmp_out_buf);
+        gmp_out_buf = NULL;
 
     }
-
-    printf ("    %14i digits\n", (int)num_bytes);
 
     for ( j = 0; j < (loop_limit-1); j++ ) {
         err_clock = clock_gettime(clock_flag, &tn_0);
-        mpz_mul (g1, g0, g0);
+        mpz_mul(g1, g0, g0);
         err_clock = clock_gettime(clock_flag, &tn_1);
         err_clock = tdiff( &delta_time, tn_0, tn_1);
-        total_time += delta_time.delta;
+        mpz_mul_time += delta_time.delta;
+        fg[j+1] = mpz_mul_time;
 
-        /* num_bytes = mpz_out_str(stdout, 10, g1); */
-        out_buf = mpz_get_str(null_ptr, 10, g1);
-        if ( out_buf == NULL ) {
-            fprintf(stderr,"FAIL : bork mpz_get_str() returns NULL\n");
-            return EXIT_FAILURE;
-        }
-        printf ("\n%3i    %s", j+2, out_buf);
-        num_bytes = strlen(out_buf);
-        printf ("\n    %14i digits\n", (int)num_bytes);
-        printf ("       t0  %7li secs %9li nsec\n", tn_0.tv_sec, tn_0.tv_nsec);
-        printf ("       t1  %7li secs %9li nsec    compute dt = %-+20.10g\n",
-                                tn_1.tv_sec, tn_1.tv_nsec, delta_time.delta);
+        /* Once upon a time we would output the whole nasty long
+         * string of digits thus :
+         *
+         *      num_bytes = mpz_out_str(stdout, 10, g1);
+         *
+         * We do not do that anymore for reasons of sanity.
+         */
 
+        err_clock = clock_gettime(clock_flag, &tn_0);
+        gmp_out_buf = mpz_get_str(null_ptr, 10, g1);
         err_clock = clock_gettime(clock_flag, &tn_1);
         err_clock = tdiff( &delta_time, tn_0, tn_1);
-        printf ("       out %7li secs %9li nsec     output dt = %-+20.10g\n",
-                                tn_1.tv_sec, tn_1.tv_nsec, delta_time.delta);
+        mpz_get_str_time += delta_time.delta;
+        mgst[j+1] = delta_time.delta;
 
-        free(out_buf);
-        out_buf = NULL;
+        strncpy(prn_buf, gmp_out_buf, 72);
+        printf("%3i    : %s", j+2, prn_buf);
+        num_bytes = strlen(gmp_out_buf);
+        if (num_bytes>72) printf("...");
+        printf("\n");
+        prn_buf[0]='\0';
 
-        mpz_set (g0, g1);
+        printf ("    %14i digits    dt = %-.9f     mgs_dt = %-.9f\n\n",
+                           (int)num_bytes, fg[j+1], mgst[j+1]);
+
+        free(gmp_out_buf);
+        gmp_out_buf = NULL;
+
+        mpz_set(g0, g1);
 
     }
 
-    mpz_clears ( g0, g1, NULL);
+    mpz_clears( g0, g1, NULL);
+    free(prn_buf);
+    prn_buf = NULL;
 
-    err_clock = tdiff( &delta_time, tn_begin, tn_1);
-    printf ("\n\nTotal computation time %-+20.10g secs\n", total_time);
-    printf ("\nWith stdout %7li secs %9li nsecs\n",
-                                           delta_time.sec, delta_time.nsec);
+    err_clock = clock_gettime(clock_flag, &tn_end);
+    err_clock = tdiff( &delta_time, tn_begin, tn_end);
+
+    printf ("\n              total mpz_mul time     %-.9f secs\n",
+                                       mpz_mul_time);
+
+    printf ("\n              total mpz_get_str time %-.9f secs\n",
+                                       mpz_get_str_time);
+
+    printf ("\n              total execute time     %-.9f secs\n",
+                                       delta_time.delta);
+
+    printf("\n------ compute time and mpz_get_str times ------\n");
+    for (j = 0; j < loop_limit; j++ ) {
+        printf ("%3i      %-.9f      %-.9f\n",j+1,fg[j],mgst[j]);
+    }
+    printf("------------------------------------------------\n");
 
     EXIT_SUCCESS;
 
