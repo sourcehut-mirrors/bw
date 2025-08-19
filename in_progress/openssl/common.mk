69a2e7f63d63e8b62245cfc5a57de7c8ebfab1b9
blastwave <dclarke@blastwave.org>
2025-08-19 16:30:42 +0000

    20250819163042 : finally a decent Makefile for FreeBSD ppc64 OpenSSL test

--- in_progress/openssl/common.mk
+++ in_progress/openssl/common.mk
@@ -1,18 +1,25 @@
-CC?=		/usr/bin/cc
-CPPFLAGS=	-D_POSIX_PTHREAD_SEMANTICS -D_LARGEFILE64_SOURCE \
+
+CC?=	/usr/bin/cc
+
+CPPFLAGS=	-D_LARGEFILE64_SOURCE \
 		-D_REENTRANT -D_XOPEN_SOURCE=600
 
-LIBS=		-lcrypto -lm
+IDIR=	/opt/bw/include
+LDIR=	/opt/bw/lib
 
-OBJS=		dgst_test.o ../../sysinfo/sysinfo.o \
-			../../sysinfo/endian.o
+LIBS=	-lcrypto -lm
+
+OBJS=	dgst_test.o ../../sysinfo/sysinfo.o \
+	../../sysinfo/endian.o
 
 .PHONY: all
 all: dgst_test
 
 dgst_test: $(OBJS)
-	$(CC) -o dgst_test $(OBJS) $(CFLAGS) $(CPPFLAGS) $(LIBS)
+	$(CC) -o dgst_test $(OBJS) $(CFLAGS) $(CPPFLAGS) \
+	-I$(IDIR) -L$(LDIR) $(LIBS)
 
 .PHONY: clean
 clean:
 	rm -f $(OBJS) dgst_test
+
