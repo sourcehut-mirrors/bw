8d1d7721f794edfa7b3a314ae19743e69c462fb4
blastwave <dclarke@blastwave.org>
2025-12-07 11:05:22 +0000

    20251207110522 : trivial OpenSSL sample code for c and s

--- in_progress/openssl/digest_test/common.mk
+++ in_progress/openssl/digest_test/common.mk
@@ -1,16 +1,15 @@
 
 CC?=	/usr/bin/cc
 
-CPPFLAGS=	-D_LARGEFILE64_SOURCE \
-		-D_XOPEN_SOURCE=600
+CPPFLAGS=	-D_LARGEFILE64_SOURCE
 
 IDIR=	/opt/bw/include
 LDIR=	/opt/bw/lib
 
 LIBS=	-lcrypto -lm
 
-OBJS=	dgst_test.o ../../sysinfo/sysinfo.o \
-	../../sysinfo/endian.o
+OBJS=	dgst_test.o ../../../sysinfo/sysinfo.o \
+		../../../sysinfo/endian.o
 
 .PHONY: all
 all: dgst_test
