af5fa795d3380fe8ae6bd82d5195dfbf131be92d
blastwave <dclarke@blastwave.org>
2026-04-20 00:50:54 +0000

    20260420005054 : output time and date in given locale

--- in_progress/grep/bug80774/test_wchar.c
+++ in_progress/grep/bug80774/test_wchar.c
@@ -14,6 +14,7 @@
    Small test code should output "towupper (0x01C5) = 0x01C4" and on
    OpenBSD it may output "towupper (0x01C5) = 0x01C".
 
+   Comment from Dennis Clarke : time and date output in requested locale
 */
 
 /*
@@ -22,7 +23,7 @@
  *                 #define _XOPEN_SOURCE 500
  *
  * NOTE: for reasons yet unknown OpenBSD 7.8 has a fit if you try
- *       to define _XOPEN_SOURCE and be damned if I know why.
+ *       to define _XOPEN_SOURCE and I do not know why.
  */
 
 #if ! defined(__OpenBSD__)
@@ -31,24 +32,27 @@
 #endif
 #endif
 
+#include <errno.h>
 #include <locale.h>
 #include <wctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 int
 main( int argc, char **argv )
 {
     /* NOTE : wchar.h - wide-character types */
     wint_t c, w;
-    char *buf;
+    char *buf, time_string[256];
 
-    /* assume a trivial POSIX locale */
-    buf = setlocale( LC_ALL, "POSIX" );
-    if ( buf == NULL ) {
-        fprintf (stderr,"FAIL : setlocale fail\n");
-        return EXIT_FAILURE;
-    }
+    struct timespec tn_now;
+    struct tm *tm_now;
+    time_t now;
+    int err_clock;
+    size_t j;
+    clockid_t clock_flag;
 
     if ( argc > 1 ) {
         printf("\nINFO : You suggest a locale of %s\n", argv[1]);
@@ -64,8 +68,39 @@ main( int argc, char **argv )
         }
         printf("     : accepted.\n");
     } else {
-        printf("\nINFO : locale is set to default \"POSIX\".\n\n");
+        printf("\nINFO : locale is set to default \"POSIX\".\n");
+        /* assume a trivial POSIX locale */
+        buf = setlocale( LC_ALL, "POSIX" );
+        if ( buf == NULL ) {
+            fprintf (stderr,"FAIL : setlocale fail\n");
+            return EXIT_FAILURE;
+        }
     }
+
+    /* output the date and time in the requested locale */
+
+    clock_flag = CLOCK_REALTIME;
+    err_clock = clock_gettime(clock_flag, &tn_now);
+    if ( err_clock != 0 ) {
+        if ( errno == EINVAL ) {
+            /* CLOCK_REALTIME shall always be implemented if
+             * the clock_gettime() function exists. */
+            fprintf(stderr,"FAIL : CLOCK_REALTIME not supported\n");
+            return EXIT_FAILURE;
+        }
+        fprintf(stderr,"FAIL : clock_gettime() fails\n");
+        return EXIT_FAILURE;
+    }
+
+    now = tn_now.tv_sec;
+    tm_now = localtime(&now);
+
+    j = strftime( time_string, 256,
+                  "%Y %m %d %A %a %B %b day=%j dow=%u week=%V\n       at %H:%M:%S %z",
+                  tm_now);
+
+    printf("\nINFO : date and time in this locale is %i bytes\n", j);
+    printf("     : %s\n\n", time_string);
 
     c = 0x01C5;
     w = towupper(c);
@@ -75,8 +110,6 @@ main( int argc, char **argv )
     printf("towupper (0x01C5) = 0x%04X\n", (int) w);
 
     printf("\n\nShould output \"towupper (0x01C5) = 0x01C4\".\n");
-    printf("Buggy stuff may output \"towupper (0x01C5) = 0x01C\"\n");
-    printf("OpenBSD 7.8 is even more strange and reports 0x01C5\n\n");
 
     return EXIT_SUCCESS;
 
