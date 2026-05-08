a4314078585c8ba9cbe17746d661ad122ee6c088
blastwave <dclarke@blastwave.org>
2026-05-08 09:39:23 +0000

    20260508093922 : a possible test for locale_time.c

---
+++ time_and_date/test_locale_time.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+IFS=' 	
+'
+
+
+./locale_time C
+./locale_time cs_CZ.utf8
+./locale_time C.utf8
+./locale_time de_DE.utf8
+./locale_time el_GR.utf8
+./locale_time en_AU.utf8
+./locale_time en_US.utf8
+./locale_time es_ES.utf8
+./locale_time fi_FI.utf8
+./locale_time he_IL.utf8
+./locale_time hu_HU.utf8
+./locale_time it_IT.utf8
+./locale_time ja_JP.utf8
+./locale_time ko_KR.utf8
+./locale_time pl_PL.utf8
+./locale_time POSIX
+./locale_time pt_BR.utf8
+./locale_time pt_PT.utf8
+./locale_time ro_RO.utf8
+./locale_time ru_RU.utf8
+./locale_time sv_SE.utf8
+./locale_time th_TH.utf8
+./locale_time zh_TW.utf8
