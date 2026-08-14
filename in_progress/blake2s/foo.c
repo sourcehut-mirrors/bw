20fad589af96ff1308118badb73c4188eed421fd
blastwave <dclarke@blastwave.org>
2026-08-14 23:44:12 +0000

    20260814234412 : helps if the parameters to blake2s are in the right order

--- in_progress/blake2s/foo.c
+++ in_progress/blake2s/foo.c
@@ -1,26 +1,35 @@
 
 /* foo.c  trivial usage of BLAKE2 hash
  *
- * which does not work .. yet
- *
  * using strict C90 compiler flags and -D_XOPEN_SOURCE 500
  *
  * $ $CC $CFLAGS $CPPFLAGS -c -o blake2s.o blake2s.c
  * $ $CC $CFLAGS $CPPFLAGS -c -o foo.o foo.c
  * $ $CC $CFLAGS $CPPFLAGS -o foo foo.o blake2s.o
- * $ ./foo
- * 8bd3ccf8b0ad6a7ff10bb345830b66d3abc3cf1d206cff81f335c7c7f1ecca96
- * 
- *        * * *    T H A T    I S    W R O N G    * * *
  *
+ *
+ * $ ./foo
+ * BLAKE2s256 hash of "foo" ( 3 bytes ) is :
+ * 08d6cad88075de8f192db097573d0e829411cd91eb6ec65e8fc16c017edfdb74
+ *
+ * Verify with OpenSSL thus :
+ * /usr/bin/printf "foo" | openssl dgst -blake2s256 -r -
+ * 08d6cad88075de8f192db097573d0e829411cd91eb6ec65e8fc16c017edfdb74 *stdin
+ *
+ * NOTE there is no "\n" on the end of the input string "foo"
  */
 
 #if ! defined (_XOPEN_SOURCE)
 #define _XOPEN_SOURCE 500
 #endif
 
-/* until we can actually get system architecture data */
+/* get system architecture data someday
+ * because we need to calloc/malloc lots
 #define PAGE_SIZE 4096
+ *
+ *  today is not that day
+ */
+
 #define OUTPUT_LEN 32
 
 #include <stdio.h>
@@ -36,7 +45,7 @@ main ( int argc, char **argv )
     /* we may add more strings into the array later */
     static const char *foo[] = { "foo", NULL };
     size_t outlen = OUTPUT_LEN;
-    uint8_t hash_out[OUTPUT_LEN];
+    unsigned char hash_out[OUTPUT_LEN];
 
     blake2s_ctx ctx;
     int error_status = 0;
@@ -48,21 +57,16 @@ main ( int argc, char **argv )
     size_t inlen = strlen(foo[0]);
 
     /* 32 bytes and the key is NULL with 0 len key */
-    error_status = blake2s(hash_out,outlen,foo[0],inlen,NULL,0);
+    error_status = blake2s(hash_out, OUTPUT_LEN,
+                           NULL, 0,
+                           foo[0], inlen);
 
     if ( error_status != 0 ) {
         fprintf(stderr,"FAIL : blake2s() bork bork bork\n");
         return EXIT_FAILURE;
     }
 
-    /* verify with OpenSSL thus :
-     *
-     * $ /usr/bin/printf "foo" | openssl dgst -blake2s256 -r
-     * 08d6cad88075de8f192db097573d0e829411cd91eb6ec65e8fc16c017edfdb74 *stdin
-     *
-     * NOTE there is no "\n" on the end of the input string "foo"
-     */
-    printf("\n");
+    printf("BLAKE2s256 hash of \"foo\" ( 3 bytes ) is :\n");
     for (j = 0; j < OUTPUT_LEN; j++) {
         printf("%02x", hash_out[j]);
     }
