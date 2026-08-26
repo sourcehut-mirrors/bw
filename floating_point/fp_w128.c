a5a3e47df336e63ddefdb62595e3790e2b53cb96
blastwave <dclarke@blastwave.org>
2026-08-26 18:23:09 +0000

    20260826182309 : toss in a license and better comments on fp_w128.c

--- floating_point/fp_w128.c
+++ floating_point/fp_w128.c
@@ -1,4 +1,47 @@
 
+/*
+ * fp_w128.c  significant loss of precision in a trivial computation
+ *
+ * ------------------------------------------------------------------
+ * Copyright (c) 2019 Dennis Clarke
+ *
+ *    Permission is hereby granted, free of charge, to any person
+ *    obtaining a copy of this software and associated documentation
+ *    files (the "Software"), to deal in the Software without
+ *    restriction, including without limitation the rights to use,
+ *    copy, modify, merge, publish, distribute, sublicense, and/or
+ *    sell copies of the Software, and to permit persons to whom the
+ *    Software is furnished to do so, subject to the following
+ *    conditions:
+ *
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+ *
+ *        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ *        KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *        WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *        PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *        OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ *        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ------------------------------------------------------------------
+ */
+
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+ *********************************************************************/
+
+#define _XOPEN_SOURCE 600
+
 /* Please see section 1.3.2 Difficult problems in
  * "Handbook of Floating Point Arithmetic", 2nd Ed.
  * ISBN 978-3-319-76525-9
@@ -11,6 +54,10 @@
  *
  * Jean-Michel Muller, "Arithmetique des Ordinateurs",
  * Masson, Paris, 1989
+ *
+ * NOTE : even a POWER9 server will begin to show
+ *        real loss of precision at around loop 27
+ *
  */
 
 #define _XOPEN_SOURCE 600
